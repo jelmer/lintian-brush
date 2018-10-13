2c88799133a32d00b046222b8ffb4122761bb4d5
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-10-13 11:50:29 +0100

    Add copyright headers.

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+# Copyright (C) 2018 Jelmer Vernooij
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 from setuptools import setup
 
@@ -13,4 +28,13 @@ setup(
             "Repository": "https://salsa.debian.org/jelmer/lintian-brush",
         },
         requires=['breezy', 'debian'],
+        entry_points = {
+            'console_scripts': ['lintian-brush=lintian_brush.__main__']
+        },
+        package_dir={
+            'lintian_brush': 'lintian_brush',
+        },
+        package_data={
+            'lintian_brush': ['../fixers/lintian/*'],
+        },
 )

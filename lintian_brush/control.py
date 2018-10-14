460dfee894d0f4e589c1ea945dcd2f39a36c6263
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-10-14 17:09:49 +0000

    Fix python3 compatibility.

--- lintian_brush/control.py
+++ lintian_brush/control.py
@@ -23,7 +23,7 @@ def update_control(path='debian/control', **kwargs):
     outf = BytesIO()
     with open(path, 'rb') as f:
         contents = f.read()
-    if "DO NOT EDIT" in contents:
+    if b"DO NOT EDIT" in contents:
         raise Exception("control file not editable")
     update_control_file(BytesIO(contents), outf, **kwargs)
     with open(path, 'wb') as f:
@@ -39,6 +39,6 @@ def update_control_file(inf, outf, source_package_cb=None, binary_package_cb=Non
             binary_package_cb(paragraph)
         if paragraph:
             if not first:
-                outf.write('\n')
+                outf.write(b'\n')
             paragraph.dump(fd=outf, encoding='utf-8')
             first = False

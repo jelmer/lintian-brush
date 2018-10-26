5e4763662307be94094d3e79e6b1b2cc1f61e2fb
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-10-26 19:31:09 +0100

    Convert missing-debian-source-format to python.

---
+++ fixers/missing-debian-source-format.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import os
+if not os.path.exists('debian/source'):
+    os.mkdir('debian/source')
+if os.path.exists('debian/source/format'):
+    # source format file already exists
+    sys.exit(0)
+
+from debian.changelog import Changelog
+with open('debian/changelog') as f:
+  ch = Changelog(f, max_blocks=1)
+
+with open('debian/source/format', 'w') as f:
+    if not ch.version.debian_revision:
+      print("3.0 (native)")
+    else:
+      print("3.0 (quilt)")
+print("Explicit specify source format.")
+print("Fixed-Lintian-Tags: missing-debian-source-format")

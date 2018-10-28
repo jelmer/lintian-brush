78ecb6b64b3c1c7faaca5b835b38a5278f32e04e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-10-28 23:56:39 +0000

    package-uses-deprecated-source-override-location: Don't error out when file is missing.

--- fixers/package-uses-deprecated-source-override-location.sh
+++ fixers/package-uses-deprecated-source-override-location.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
-if [ ! -f debian/source.lintian-overrides ]; then
-    echo "File missing" >&2
-    exit 1
+if [ -f debian/source.lintian-overrides ]; then
+    if [ ! -d debian/source ]; then
+        mkdir debian/source
+    fi
+    mv debian/source.lintian-overrides debian/source/lintian-overrides
 fi
-if [ ! -d debian/source ]; then
-    mkdir debian/source
-fi
-mv debian/source.lintian-overrides debian/source/lintian-overrides
 echo "Move source package lintian overrides to debian/source."
 echo "Fixed-Lintian-Tags: package-uses-deprecated-source-override-location"

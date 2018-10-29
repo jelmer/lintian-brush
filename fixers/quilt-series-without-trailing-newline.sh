f1de685b3dc68f4f8877faa62e877ab17dc51d71
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-10-29 00:02:37 +0000

    Cope with missing debian/patches/series in quilt-series-without-trailing-newline.

--- fixers/quilt-series-without-trailing-newline.sh
+++ fixers/quilt-series-without-trailing-newline.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-[ -n "$(tail -c1 debian/patches/series)" ] && echo >> debian/patches/series
+if [ -f debian/patches/series ]; then
+	[ -n "$(tail -c1 debian/patches/series)" ] && echo >> debian/patches/series
+fi
 echo "Add missing trailing newline in debian/patches/series."
 echo "Fixed-Lintian-Tags: quilt-series-without-trailing-newline"

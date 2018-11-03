06a9d5c4c712d05e64ee5c65fde6fea416796b32
Jelmer Vernooĳ <jelmer@blodeuwedd>
2018-11-03 16:58:12 +0000

    file-contains-trailing-whitespace: Also trim empty lines from the ends of files.

--- fixers/file-contains-trailing-whitespace.sh
+++ fixers/file-contains-trailing-whitespace.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 sed -i -e 's@[[:space:]]*$@@g' debian/control* debian/changelog
+sed -i -e 's@[ ]*$@@g' debian/rules
+sed -i -e :a -e '/^\n*$/{$d;N;};/\n$/ba' debian/rules debian/changelog debian/control
 echo "Trim trailing whitespace."
 echo "Fixed-Lintian-Tags: file-contains-trailing-whitespace"

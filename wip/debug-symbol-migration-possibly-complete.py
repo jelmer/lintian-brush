dc48cfd9cd1e31362e47bd4e53fa085130530577
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-11-04 16:02:17 +0000

    update notes for debug-symbol-migration-possibly-complete

--- wip/debug-symbol-migration-possibly-complete.py
+++ wip/debug-symbol-migration-possibly-complete.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python
+
+# TODO: check if there was a -dbgsym version of the package included in stretch
+# TODO: check if that package has a Replaces for the dbg package.
+
+# TODO: Where do we get apt informatoin for stretch?

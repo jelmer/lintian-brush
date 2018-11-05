aa050993cbb036151381e31c539f64fab29933f5
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-11-05 19:16:00 +0000

    Require changes in debian/rules for useless-autoreconf-build-depends.

--- fixers/useless-autoreconf-build-depends.py
+++ fixers/useless-autoreconf-build-depends.py
@@ -2,6 +2,8 @@
 from debian.changelog import Version
 from io import StringIO
 import re
+import sys
+
 from lintian_brush.control import (
     drop_dependency,
     ensure_minimum_version,
@@ -17,20 +19,27 @@ def bump_debhelper(control):
         control["Build-Depends"],
         "dh-autoreconf")
 
-
-update_control(source_package_cb=bump_debhelper)
-
+changed = False
 outf = StringIO()
 with open('debian/rules', 'r') as f:
     for line in f:
+        orig_line = line
         line = re.sub(" --with[ =]autoreconf( .+|)$", "\\1", line)
         line = re.sub(" --with[ =]autoreconf,", " --with=", line)
         line = re.sub(" --with[ =]([^ ]),autoreconf([ ,])", " --with=\\1\\2",
                       line)
+        if orig_line != line:
+            changed = True
         outf.write(line)
+
+
+if not changed:
+    sys.exit(2)
 
 with open('debian/rules', 'w') as f:
     f.write(outf.getvalue())
+
+update_control(source_package_cb=bump_debhelper)
 
 print("Drop unnecessary dependency on dh-autoconf.")
 print("Fixed-Lintian-Tags: useless-autoreconf-build-depends")

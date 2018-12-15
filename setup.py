08e4eba94eafb8988c4b459ae8f979fc1f7b07f7
Jelmer Vernooĳ <jelmer@jelmer.uk>
2018-12-15 23:25:00 +0000

    Fix version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name="lintian-brush",
-    version="0.9",
+    version="0.10",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["lintian_brush"],

abadee65c9f1ce61a5c4ddb32e000b4a165c81c7
Dmitry Bogatov <KAction@debian.org>
2019-02-10 22:45:28 +0000

    Do not throw exception on packages without d/compat
    Previosly, following error message was triggered by missing `debian/compat':
    
     Some fixer scripts failed to run: {'package-needs-versioned-debhelper-build-depends'}. Run with --verbose for details.
    
    Running with --verbose exposed Python exceptions to user, which is not
    supposed to understand Python.

--- fixers/package-needs-versioned-debhelper-build-depends.py
+++ fixers/package-needs-versioned-debhelper-build-depends.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 
+import sys
 from lintian_brush.control import (
     ensure_minimum_version,
     update_control,
     )
-
-
-with open('debian/compat', 'r') as f:
-    minimum_version = f.read().strip()
-
-
 def bump_debhelper(control):
     control["Build-Depends"] = ensure_minimum_version(
             control["Build-Depends"],
             "debhelper",
             "%s~" % minimum_version)
 
+# Debian source package is not obliged to contain `debian/compat'.
+# Firstly, it may not use debhelper; secondly it may use modern
+# `debhelper-compat' dependency style.
+try:
+    with open('debian/compat', 'r') as f:
+        minimum_version = f.read().strip()
+    update_control(source_package_cb=bump_debhelper)
+except FileNotFoundError:
+    minimum_version = 9999 # Not used anyway.
 
-update_control(source_package_cb=bump_debhelper)
 print("Bump debhelper dependency to >= %s, since that's what is "
       "used in debian/compat." % minimum_version)
 print("Fixed-Lintian-Tags: package-needs-versioned-debhelper-build-depends")

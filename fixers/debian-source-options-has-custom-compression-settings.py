395c8c21ea30e4e432be501826c8578c7203f74b
Jelmer Vernooĳ <jelmer@debian.org>
2019-02-12 03:13:54 +0000

    Add fixer script for debian-source-options-has-custom-compression- settings.

---
+++ fixers/debian-source-options-has-custom-compression-settings.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+import sys
+
+try:
+    with open('debian/source/options', 'r') as f:
+        oldlines = list(f.readlines())
+except FileNotFoundError:
+    sys.exit(0)
+
+newlines = []
+for line in oldlines:
+    (key, value) = line.split('=')
+    if key.strip() == 'compression':
+        print("Drop custom source compression.")
+        continue
+    if key.strip() == 'compression-level':
+        print("Drop custom source compression level.")
+        continue
+    newlines.append(line)
+
+with open('debian/source/options', 'w') as f:
+    f.writelines(newlines)
+
+print(
+    "Fixed-Lintian-Tags: debian-source-options-has-custom-compression-settings"
+    )

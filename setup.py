5a76b4168a7e8f67b2180f3aa12f6789a42303ef
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-02-16 16:03:03 +0000

    Add fixer for debian-changelog-file-contains-obsolete-user-emacs- settings.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name="lintian-brush",
-    version="0.13",
+    version="0.14",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["lintian_brush"],

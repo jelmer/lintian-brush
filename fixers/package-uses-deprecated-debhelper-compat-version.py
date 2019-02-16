875dfcb52772e80f113910625218bd5738924016
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-02-16 18:29:25 +0000

    Clarify variable names.

--- fixers/package-uses-deprecated-debhelper-compat-version.py
+++ fixers/package-uses-deprecated-debhelper-compat-version.py
@@ -12,18 +12,18 @@ minimum_debhelper_version = 9
 
 compat_release = os.environ.get('COMPAT_RELEASE', 'sid')
 if compat_release == 'sid':
-    new_debhelper_version = 12
+    new_debhelper_compat_version = 12
 elif compat_release == 'stretch':
-    new_debhelper_version = 10
+    new_debhelper_compat_version = 10
 else:
-    new_debhelper_version = minimum_debhelper_version
+    new_debhelper_compat_version = minimum_debhelper_version
 
 if os.path.exists('debian/compat'):
     with open('debian/compat', 'r') as f:
-        current_debhelper_version = int(f.read().strip())
-    if current_debhelper_version < new_debhelper_version:
+        current_debhelper_compat_version = int(f.read().strip())
+    if current_debhelper_compat_version < new_debhelper_compat_version:
         with open('debian/compat', 'w') as f:
-            f.write('%s\n' % new_debhelper_version)
+            f.write('%s\n' % new_debhelper_compat_version)
     else:
         # Nothing to do
         sys.exit(2)
@@ -35,16 +35,16 @@ def bump_debhelper(control):
     control["Build-Depends"] = ensure_minimum_version(
             control["Build-Depends"],
             "debhelper",
-            "%d~" % new_debhelper_version)
+            "%d~" % new_debhelper_compat_version)
 
 
 update_control(source_package_cb=bump_debhelper)
-if current_debhelper_version < minimum_debhelper_version:
+if current_debhelper_compat_version < minimum_debhelper_version:
     kind = "deprecated"
     tag = "package-uses-deprecated-debhelper-compat-version"
 else:
     kind = "old"
     tag = "package-uses-old-debhelper-compat-version"
 print("Bump debhelper from %s %s to %s." % (
-    kind, current_debhelper_version, new_debhelper_version))
+    kind, current_debhelper_compat_version, new_debhelper_compat_version))
 print("Fixed-Lintian-Tags: %s" % tag)

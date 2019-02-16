9c2e5f2f9da157a9d57526686256d49f65a10298
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-02-16 15:24:36 +0000

    Only update to new standards-version if the upgrade path is known to be good.

--- fixers/out-of-date-standards-version.py
+++ fixers/out-of-date-standards-version.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 
+
 from lintian_brush.control import update_control
 
 
+# Dictionary mapping source and target versions
+upgrade_path = {
+    "4.2.0": "4.2.1",
+}
+
+
 def bump_standards_version(control):
-    control["Standards-Version"] = "4.3.0"
+    current_version = control["Standards-Version"]
+    while current_version in upgrade_path:
+        current_version = upgrade_path[current_version]
+    control["Standards-Version"] = current_version
 
 
 update_control(source_package_cb=bump_standards_version)

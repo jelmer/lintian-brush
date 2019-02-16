f36be6fd54155064a1fa8ee541e48eb9b6655ae6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-02-16 16:40:11 +0000

    Actually add fixer for debian-changelog-file-contains-obsolete-user-emacs-settings

---
+++ fixers/debian-changelog-file-contains-obsolete-user-emacs-settings.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+perl -pe 'BEGIN{undef $/;} s/(Local variables:.*?)
+add-log-mailing-address: .*
+(End:)
+/
+\1
+\2
+/smg' -i debian/changelog
+
+perl -pe 'BEGIN{undef $/;} s/([\n]*Local variables:.*?)
+mode: debian-changelog
+(End:[\n]+)/
+/smg' -i debian/changelog
+
+echo "Drop no longer supported add-log-mailing-address setting from debian/changelog."
+echo "Fixes-Lintian-Tags: debian-changelog-file-contains-obsolete-user-emacs-setting"

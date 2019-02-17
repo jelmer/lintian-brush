b4dae2282d3382782b3ab26692509aacdb64cdd6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-02-17 00:47:06 +0000

    Add fixer for build-depends-on-build-essential.

---
+++ fixers/build-depends-on-build-essential.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from lintian_brush.control import (
+    drop_dependency,
+    update_control,
+    )
+
+
+def drop_build_essential(control):
+    control["Build-Depends"] = drop_dependency(
+        control["Build-Depends"], "build-essential")
+
+
+update_control(source_package_cb=drop_build_essential)
+print("Drop unnecessary dependency on build-essential.")
+print("Fixed-Lintian-Tags: build-depends-on-build-essential")

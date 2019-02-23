4535c0534dde17e2b4d9a8827c52f5dff1e2b483
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-02-23 03:46:37 +0000

    Bump version to 0.16.

--- lintian_brush/__init__.py
+++ lintian_brush/__init__.py
@@ -43,7 +43,7 @@ from breezy.transform import revert
 from debian.deb822 import Deb822
 
 
-__version__ = (0, 15)
+__version__ = (0, 16)
 version_string = '.'.join(map(str, __version__))
 SUPPORTED_CERTAINTIES = ['certain', 'possible', None]
 
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name="lintian-brush",
-    version="0.15",
+    version="0.16",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["lintian_brush"],

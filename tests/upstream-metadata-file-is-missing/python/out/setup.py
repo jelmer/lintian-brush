e4b80d090e63b3a63968f1ef8f612f994f200999
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-05-04 21:45:54 +0000

    Check setup.py for metadata.

---
+++ tests/upstream-metadata-file-is-missing/python/out/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="blah",
+    version="0.16",
+    packages=["blah"],
+    url="https://salsa.debian.org/jelmer/lintian-brush",
+    description="Automatic lintian issue fixer",
+    project_urls={
+        "Repository": "https://salsa.debian.org/jelmer/lintian-brush",
+    },
+)

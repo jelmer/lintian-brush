da4ce024e3511363e562f67aeca5ee00ba028ef5
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-05-04 21:11:16 +0000

    use ruamel-yaml.

--- fixers/upstream-metadata-file-is-missing.py
+++ fixers/upstream-metadata-file-is-missing.py
@@ -5,24 +5,33 @@
 
 import os
 import sys
-import yaml
+import ruamel.yaml
 
 from debian.deb822 import Deb822
 with open('debian/control', 'r') as f:
     control = Deb822(f)
 
-fields = {}
-if 'XS-Go-Import-Path' in control:
-    fields['Repository'] = 'https://' + control['XS-Go-Import-Path']
+try:
+    with open('debian/upstream/metadata', 'r') as f:
+        inp = f.read()
+except FileNotFoundError:
+    code = {}
+else:
+    code = ruamel.yaml.load(inp, ruamel.yaml.RoundTripLoader)
 
-if not fields:
+
+if 'Repository' not in code:
+    if 'XS-Go-Import-Path' in control:
+        code['Repository'] = 'https://' + control['XS-Go-Import-Path']
+
+if not code:
     sys.exit(0)
 
 if not os.path.isdir('debian/upstream'):
     os.mkdir('debian/upstream')
 
 with open('debian/upstream/metadata', 'w') as f:
-    yaml.dump(fields, f, default_flow_style=False)
+    ruamel.yaml.dump(code, f, Dumper=ruamel.yaml.RoundTripDumper)
 
 print('Set upstream metadata fields.')
 print('Certainty: possible')

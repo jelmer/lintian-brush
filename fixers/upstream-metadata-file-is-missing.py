47a997ab9c60c513739c215d8c07c66e64db6d38
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-05-04 21:45:54 +0000

    Check setup.py for metadata.

--- fixers/upstream-metadata-file-is-missing.py
+++ fixers/upstream-metadata-file-is-missing.py
@@ -6,6 +6,8 @@
 import os
 import sys
 import ruamel.yaml
+import subprocess
+import tempfile
 
 from debian.deb822 import Deb822
 with open('debian/control', 'r') as f:
@@ -23,6 +25,24 @@ else:
 if 'Repository' not in code:
     if 'XS-Go-Import-Path' in control:
         code['Repository'] = 'https://' + control['XS-Go-Import-Path']
+
+    if os.path.exists('setup.py'):
+        with tempfile.TemporaryDirectory() as td:
+            subprocess.call(
+                ['python', os.path.abspath('setup.py'), 'dist_info'], cwd=td,
+                stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+            [name] = os.listdir(td)
+            with open(os.path.join(td, name, 'PKG-INFO'), 'r') as f:
+                python_info = [
+                    l.rstrip('\n').split(': ', 1) for l in f.readlines()]
+        for key, value in python_info:
+            if key == 'Name':
+                code['Name'] = value
+            if key == 'Project-URL':
+                url_type, url = value.split(', ')
+                if url_type in ('GitHub', 'Repository'):
+                    code['Repository'] = url
+
 
 if not code:
     sys.exit(0)

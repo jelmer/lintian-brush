85b710aabeb1ff6220f5285d5f10df80fc14d1d7
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-07-20 11:58:30 +0000

    Bump version strings.

--- lintian_brush/__init__.py
+++ lintian_brush/__init__.py
@@ -43,7 +43,7 @@ from breezy.transform import revert
 from debian.deb822 import Deb822
 
 
-__version__ = (0, 16)
+__version__ = (0, 17)
 version_string = '.'.join(map(str, __version__))
 SUPPORTED_CERTAINTIES = ['certain', 'possible', None]
 DEFAULT_MINIMUM_CERTAINTY = 'certain'
--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name="lintian-brush",
-    version="0.16",
+    version="0.17",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["lintian_brush"],

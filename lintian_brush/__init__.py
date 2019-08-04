61ea9357c909d18b26615f17de6f5bd0fa8a2e8d
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-04 23:39:27 +0000

    Bump version to 0.21.

--- lintian_brush/__init__.py
+++ lintian_brush/__init__.py
@@ -46,7 +46,7 @@ from breezy.transform import revert
 from debian.deb822 import Deb822
 
 
-__version__ = (0, 20)
+__version__ = (0, 21)
 version_string = '.'.join(map(str, __version__))
 SUPPORTED_CERTAINTIES = ['certain', 'possible', None]
 DEFAULT_MINIMUM_CERTAINTY = 'certain'

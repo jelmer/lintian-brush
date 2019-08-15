4c91be2faa4941a9a4a0e3db7ba264253484648e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-15 02:39:54 +0000

    Add java team repo location.

--- lintian_brush/salsa.py
+++ lintian_brush/salsa.py
@@ -30,6 +30,7 @@ MAINTAINER_EMAIL_MAP = {
     'debian-science-maintainers@lists.alioth.debian.org': 'science-team',
     'pkg-perl-maintainers@lists.alioth.debian.org':
         'perl-team/modules/packages',
+    'pkg-java-maintainers@lists.alioth.debian.org': 'java-team',
     }
 
 

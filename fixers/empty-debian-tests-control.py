7b57f02f0a6d3151794e37d2021b43707b50a4c6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-19 08:44:20 +0000

    Remove debian/tests if it's empty.

--- fixers/empty-debian-tests-control.py
+++ fixers/empty-debian-tests-control.py
@@ -6,6 +6,8 @@ try:
     with open('debian/tests/control', 'r') as f:
         if f.read().strip() == '':
             os.unlink('debian/tests/control')
+            if not os.listdir('debian/tests'):
+                os.rmdir('debian/tests')
 except FileNotFoundError:
     pass
 

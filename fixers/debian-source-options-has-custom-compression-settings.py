a21ca10ff3c2e5d4a775f13cb7735f13ad69a542
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-19 08:33:30 +0000

    In debian-source-options-has-custom-compression-settings.py, don't crash on options that are not key/value.

--- fixers/debian-source-options-has-custom-compression-settings.py
+++ fixers/debian-source-options-has-custom-compression-settings.py
@@ -13,14 +13,18 @@ for line in oldlines:
     if line.lstrip().startswith('#'):
         newlines.append(line)
         continue
-    (key, value) = line.split('=')
-    if key.strip() == 'compression':
-        print("Drop custom source compression.")
-        continue
-    if key.strip() == 'compression-level':
-        print("Drop custom source compression level.")
-        continue
-    newlines.append(line)
+    try:
+        (key, value) = line.split('=', 1)
+    except ValueError:
+        newlines.append(line)
+    else:
+        if key.strip() == 'compression':
+            print("Drop custom source compression.")
+            continue
+        if key.strip() == 'compression-level':
+            print("Drop custom source compression level.")
+            continue
+        newlines.append(line)
 
 with open('debian/source/options', 'w') as f:
     f.writelines(newlines)

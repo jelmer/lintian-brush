7a0ee127505dcf741ba7d8839285344a8fd61576
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-20 18:22:00 +0000

    Add fixer for unused-build-dependency-on-cdbs.

---
+++ fixers/unused-build-dependency-on-cdbs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from lintian_brush.control import drop_dependency, update_control
+
+with open('debian/rules', 'r') as f:
+    for line in f:
+        if '/usr/share/cdbs/' in line:
+            uses_cdbs = True
+            break
+    else:
+        uses_cdbs = False
+
+if not uses_cdbs:
+    def drop_cdbs(control):
+        control["Build-Depends"] = drop_dependency(
+            control["Build-Depends"], "cdbs")
+    update_control(source_package_cb=drop_cdbs)
+
+print("Drop unused build-dependency on cdbs.")
+print("Fixed-Lintian-Tags: unused-build-dependency-on-cdbs")

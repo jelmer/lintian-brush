a556c1c829936512db59f70fe9a730924241f9d9
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-08-31 23:59:51 +0000

    Add fixer debian-control-has-unusual-field-spacing.

---
+++ fixers/debian-control-has-unusual-field-spacing.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+from lintian_brush.reformatting import check_generated_file
+
+check_generated_file('debian/control')
+
+
+def rewrite_line(line):
+    if not line.strip():
+        return line
+    if line.startswith(b' ') or line.startswith(b'\t'):
+        return line
+    (key, value) = line.split(b':', 1)
+    if not value.strip():
+        return line
+    return b'%s: %s\n' % (key, value.lstrip().rstrip(b'\n'))
+
+
+lines = []
+changed = False
+with open('debian/control', 'rb') as f:
+    for oldline in f:
+        newline = rewrite_line(oldline)
+        if newline != oldline:
+            changed = True
+        lines.append(newline)
+
+if changed:
+    with open('debian/control', 'wb') as f:
+        f.writelines(lines)
+    print('Strip unusual field spacing from debian/control.')
+    print('Fixed-Lintian-Tags: debian-control-has-unusual-field-spacing')

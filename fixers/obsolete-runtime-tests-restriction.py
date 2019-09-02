8339bc9f51a244bed5ee15c56c8b1a54bcb55317
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-09-02 00:23:13 +0000

    Add fixer for obsolete-runtime-tests-restriction.

---
+++ fixers/obsolete-runtime-tests-restriction.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+from lintian_brush.deb822 import update_deb822
+
+
+DEPRECATED_RESTRICTIONS = ['needs-recommends']
+removed_restrictions = []
+
+
+def drop_deprecated_feature(paragraph):
+    restrictions = paragraph.get('Restrictions', '').split(',')
+    if restrictions == ['']:
+        return
+    for i, restriction in enumerate(list(restrictions)):
+        if restriction.strip() in DEPRECATED_RESTRICTIONS:
+            del restrictions[i]
+            removed_restrictions.append(restriction.strip())
+    paragraph['Restrictions'] = ','.join(restrictions).lstrip()
+    if not paragraph['Restrictions'].strip():
+        del paragraph['Restrictions']
+
+
+update_deb822(
+    paragraph_cb=drop_deprecated_feature, path='debian/tests/control')
+
+
+print('Drop deprecated restrictions %s. See '
+      'https://salsa.debian.org/ci-team/autopkgtest/tree/'
+      'master/doc/README.package-tests.rst' % ', ' .join(removed_restrictions))
+print('Fixed-Lintian-Tags: obsolete-runtime-tests-restriction')
+if 'needs-recommends' in removed_restrictions:
+    # This is Certainty: possible, since the package may actually rely on the
+    # (direct? indirect?) recommends, in which case we'd want to add them to
+    # Depends.
+    print('Certainty: possible')

ea04281742aedd66d24d887518c8950362610e53
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-09-02 00:52:18 +0000

    Add fixer for debian-changelog-has-wrong-day-of-week.

---
+++ fixers/debian-changelog-has-wrong-day-of-week.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+import email.utils
+from lintian_brush.changelog import update_changelog
+
+versions = []
+
+
+def fix_dow(block):
+    dt = email.utils.parsedate_to_datetime(block.date)
+    if dt is None:
+        # Can't interpret the date. Just ignore..
+        return
+    newdate = email.utils.format_datetime(dt)
+    if newdate[:3] != block.date[:3]:
+        block.date = newdate
+        versions.append(block.version)
+
+
+update_changelog(block_cb=fix_dow)
+
+print('Fix day-of-week for changelog entries %s.'
+      % ', '.join([str(v) for v in versions]))
+print('Fixed-Lintian-Tags: debian-changelog-has-wrong-day-of-week')

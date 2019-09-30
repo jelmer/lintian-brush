303e9b8134dd5c9fc09f9b362269a5c397458281
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-09-30 01:23:21 +0000

    Revert "Bump version."
    This reverts commit 9b3ac32783a8444b902ed013bf8aba14ef616a38.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name="lintian-brush",
-    version="0.34",
+    version="0.33",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["lintian_brush"],

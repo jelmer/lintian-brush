b6e63e426b67a4127db40d59bd958af18695c3b1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-10-05 15:16:55 +0000

    Add fixer debhelper-compat-wrong-field.

---
+++ fixers/debhelper-compat-wrong-field.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+from lintian_brush.control import (
+    update_control,
+    get_relation,
+    add_dependency,
+    drop_dependency,
+    )
+
+
+def move_debhelper_compat(control):
+    try:
+        pos, old = get_relation(
+            control.get('Build-Depends-Indep', []), 'debhelper-compat')
+    except KeyError:
+        return
+    control['Build-Depends'] = add_dependency(
+        control.get('Build-Depends', []), old)
+    control['Build-Depends-Indep'] = drop_dependency(
+        control.get('Build-Depends-Indep', []), 'debhelper-compat')
+    if not control['Build-Depends-Indep'].strip():
+        del control['Build-Depends-Indep']
+
+
+update_control(source_package_cb=move_debhelper_compat)
+print('Move debhelper-compat from Build-Depends-Indep to Build-Depends.')

8865a9a67a0fcdd7ed902d144ad56f42f50a8d35
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-10-07 17:40:34 +0000

    Add fixer copyright-continued-lines-with-space.

---
+++ fixers/copyright-continued-lines-with-space.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+import sys
+
+EXPECTED_HEADER = (
+    b'Format: '
+    b'https://www.debian.org/doc/packaging-manuals/copyright-format/1.0')
+
+lines = []
+with open('debian/copyright', 'rb') as f:
+    line = f.readline()
+    if line.rstrip().rstrip(b'/') != EXPECTED_HEADER:
+        sys.exit(0)
+    lines.append(line)
+    for line in f:
+        if line.startswith(b'\t'):
+            line = b' ' + line[1:]
+        lines.append(line)
+
+with open('debian/copyright', 'wb') as f:
+    f.writelines(lines)
+
+print('debian/copyright: Use spaces rather than tabs in continuation lines.')

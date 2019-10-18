af27655de9847165f2065ad3c363c7f21181dde6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-10-18 17:38:04 +0000

    Add missing dependency on libdebhelper-perl for autopkgtest.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name="lintian-brush",
-    version="0.37",
+    version="0.38",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["lintian_brush"],

b8d59f9461eae8c60c7370db40c9790dad2d2970
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-10-19 13:23:45 +0000

    copyright-continued-lines-with-space: allow copyright file to be missing.

--- fixers/copyright-continued-lines-with-space.py
+++ fixers/copyright-continued-lines-with-space.py
@@ -6,21 +6,21 @@ EXPECTED_HEADER = (
     b'Format: '
     b'https://www.debian.org/doc/packaging-manuals/copyright-format/1.0')
 
-lines = []
-with open('debian/copyright', 'rb') as f:
-    line = f.readline()
-    if line.rstrip().rstrip(b'/') != EXPECTED_HEADER:
-        sys.exit(0)
-    lines.append(line)
-    for line in f:
-        if line.startswith(b'\t'):
-            line = b' ' + line[1:]
-        lines.append(line)
-
 try:
-    with open('debian/copyright', 'wb') as f:
-        f.writelines(lines)
+    lines = []
+    with open('debian/copyright', 'rb') as f:
+        line = f.readline()
+        if line.rstrip().rstrip(b'/') != EXPECTED_HEADER:
+            sys.exit(0)
+        lines.append(line)
+        for line in f:
+            if line.startswith(b'\t'):
+                line = b' ' + line[1:]
+            lines.append(line)
 except FileNotFoundError:
     pass
+else:
+    with open('debian/copyright', 'wb') as f:
+        f.writelines(lines)
 
 print('debian/copyright: Use spaces rather than tabs in continuation lines.')

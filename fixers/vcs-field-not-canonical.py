308886ed4085990c1adb5df1519a0fe3b89b5f62
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-11-09 21:05:42 +0000

    Update canonical Git URLs on Salsa.

--- fixers/vcs-field-not-canonical.py
+++ fixers/vcs-field-not-canonical.py
@@ -2,6 +2,7 @@
 
 from lintian_brush.control import update_control
 import re
+from urllib.parse import urlparse, urlunparse
 
 
 def canonicalize_vcs_browser_url(url):
@@ -30,8 +31,19 @@ def canonicalize_vcs_browser_url(url):
     return url
 
 
+def canonical_vcs_git_url(url):
+    parsed_url = urlparse(url)
+    if parsed_url.netloc == 'salsa.debian.org':
+        if not parsed_url.path.rstrip('/').endswith('.git'):
+            parsed_url = parsed_url._replace(
+                path=parsed_url.path.rstrip('/') + '.git')
+        return urlunparse(parsed_url)
+    return url
+
+
 canonicalize_vcs = {
     'Browser': canonicalize_vcs_browser_url,
+    'Git': canonical_vcs_git_url,
 }
 
 fields = set()

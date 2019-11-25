218163ead1822ceb07ff35311022ac658de04bd6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-11-25 09:59:36 +0000

    dh-quilt-addon-but-quilt-source-format: only drop quilt if package is on the 3.0 (quilt) format.

--- fixers/dh-quilt-addon-but-quilt-source-format.py
+++ fixers/dh-quilt-addon-but-quilt-source-format.py
@@ -11,7 +11,12 @@ def drop_quilt_with(line, target):
     return dh_invoke_drop_with(line, b'quilt')
 
 
-update_rules(drop_quilt_with)
+try:
+    with open('debian/source/format', 'r') as f:
+        if f.read().strip() == '3.0 (quilt)':
+            update_rules(drop_quilt_with)
+except FileNotFoundError:
+    pass
 
 
 print("Don't specify --with=quilt, since package uses "

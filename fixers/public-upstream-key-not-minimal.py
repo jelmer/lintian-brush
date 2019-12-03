941c992c1762c0ec75aac4c283c33e08c46968f6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-12-03 21:13:33 +0000

    Don't strip comments when minimizing upstream keys. Closes: #942719

--- fixers/public-upstream-key-not-minimal.py
+++ fixers/public-upstream-key-not-minimal.py
@@ -9,17 +9,26 @@ import tempfile
 gpg = shlex.split(os.environ.get('GPG', 'gpg'))
 
 
-def run_gpg(args, keyring, homedir):
+KEY_BLOCK_START = b'-----BEGIN PGP PUBLIC KEY BLOCK-----'
+KEY_BLOCK_END = b'-----END PGP PUBLIC KEY BLOCK-----'
+
+
+def run_gpg(args, keyring, homedir, stdin=None):
     argv = (
         gpg +
         ['--homedir', homedir, '--quiet', '--no-default-keyring',
          '--keyring=%s' % keyring] +
         args)
     try:
-        return subprocess.check_output(argv)
+        p = subprocess.Popen(
+            argv, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
     except FileNotFoundError:
         # No gpg, no dice.
         sys.exit(2)
+    (stdout, stderr) = p.communicate(stdin, timeout=5)
+    if p.returncode != 0:
+        raise Exception('gpg failed')
+    return stdout
 
 
 def gpg_export(options, keyring, homedir):
@@ -28,12 +37,19 @@ def gpg_export(options, keyring, homedir):
         keyring=keyring, homedir=homedir)
 
 
-def gpg_export_minimal(target, keyring, homedir):
-    minimal = gpg_export(['export-minimal'], keyring=keyring, homedir=homedir)
-    full = gpg_export([], keyring=keyring, homedir=homedir)
-    if minimal != full:
-        with open(target, 'wb') as f:
-            f.write(minimal)
+def minimize_key_block(key):
+    with tempfile.TemporaryDirectory() as td:
+        keyring = os.path.join(td, 'keyring.pgp')
+        homedir = os.path.join(td, 'home')
+        os.mkdir(homedir, 0o700)
+        run_gpg(['--import'], keyring=keyring, homedir=homedir, stdin=key)
+        minimal = gpg_export(
+            ['export-minimal'], keyring=keyring, homedir=homedir)
+        full = gpg_export([], keyring=keyring, homedir=homedir)
+        if minimal == full:
+            return key
+        else:
+            return minimal
 
 
 for p in [
@@ -41,12 +57,25 @@ for p in [
         'debian/upstream/signing-key.pgp',
         'debian/upstream-signing-key.pgp']:
     if os.path.exists(p):
-        with tempfile.TemporaryDirectory() as td:
-            keyring = os.path.join(td, 'keyring.pgp')
-            homedir = os.path.join(td, 'home')
-            os.mkdir(homedir)
-            run_gpg(['--import', p], keyring=keyring, homedir=homedir)
-            gpg_export_minimal(p, keyring=keyring, homedir=homedir)
+        out = []
+        key = None
+        with open(p, 'rb') as f:
+            for line in f:
+                if line.strip() == KEY_BLOCK_START:
+                    key = [line]
+                elif line.strip() == KEY_BLOCK_END:
+                    key.append(line)
+                    key = minimize_key_block(b''.join(key)).splitlines(True)
+                    out.extend(key)
+                    key = None
+                elif key is not None:
+                    key.append(line)
+                else:
+                    out.append(line)
+            if key:
+                raise Exception('Key block without end')
+        with open(p, 'wb') as f:
+            f.writelines(out)
 
 
 print("Re-export upstream signing key without extra signatures.")

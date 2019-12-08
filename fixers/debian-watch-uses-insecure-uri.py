f777f61ea7d735e883f613151a68990209b538c6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-12-08 13:59:04 +0000

    convert debian-watch-uses-insecure-uri to Python.

---
+++ fixers/debian-watch-uses-insecure-uri.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+import os
+import subprocess
+import sys
+
+from lintian_brush.watch import parse_watch_file
+
+if not os.path.exists('debian/watch'):
+    sys.exit(0)
+
+
+def watchfile_has_http():
+    with open('debian/watch', 'r') as f:
+        wf = parse_watch_file(f)
+
+    for entry in wf:
+        if entry.url.startswith('http://'):
+            return True
+    else:
+        # No plain HTTP URLs
+        return False
+
+
+if not watchfile_has_http():
+    sys.exit(0)
+
+
+# We hardcode the replacements for some sites, since these testsuite uses
+# these. The method below (involving uscan) doesn't work from e.g. sbuild
+# hosts.
+with open('debian/watch', 'r') as f:
+    old = f.read()
+
+new = old
+for hostname in ['code.launchpad.net', 'launchpad.net', 'ftp.gnu.org']:
+    new = new.replace('http://%s/' % hostname, 'https://%s/' % hostname)
+
+if new != old:
+    with open('debian/watch', 'w') as f:
+        f.write(new)
+
+print("Use secure URI in debian/watch.")
+print("Fixed-Lintian-Tags: debian-watch-uses-insecure-uri")
+
+if not watchfile_has_http():
+    sys.exit(0)
+
+
+if os.environ.get('NET_ACCESS', 'disallow') == 'disallow':
+    sys.exit(0)
+
+
+def run_uscan_dehs():
+    return subprocess.check_output(
+        ['uscan', '--dehs', '--report'], stderr=subprocess.PIPE)
+
+
+before = run_uscan_dehs()
+
+with open('debian/watch', 'r') as f:
+    old = f.read()
+new = old.replace('http://', 'https://')
+with open('debian/watch', 'w') as f:
+    f.write(new)
+
+after = run_uscan_dehs()
+
+# uscan creates backup files.
+for path in [
+        'debian/upstream/signing-key.pgp.backup',
+        'debian/upstream-signing-key.pgp.backup']:
+    if os.path.exists(path):
+        os.unlink(path)
+
+# Make sure that reports are same up to http/https substitution in URL.
+if before.replace(b'http://', b'https://') != after:
+    # Couldn't do anything :(
+    sys.exit(2)

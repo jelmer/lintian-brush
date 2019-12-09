1833b26ed4b5948ff396fb283f58c06a23e3eb26
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-12-09 22:31:06 +0000

    Mark debian-rules-contains-unnecessary-get-orig-source-target rule as just possible when complex.

--- fixers/debian-rules-contains-unnecessary-get-orig-source-target.py
+++ fixers/debian-rules-contains-unnecessary-get-orig-source-target.py
@@ -2,9 +2,17 @@
 
 from lintian_brush.rules import update_rules
 
+certainty = None
+
 
 def drop_get_orig_source(rule):
+    global certainty
     if rule.has_target(b'get-orig-source'):
+        commands = rule.commands()
+        if [b'uscan'] == [c.split(b' ')[0] for c in commands]:
+            certainty = 'certain'
+        else:
+            certainty = 'possible'
         rule.clear()
 
 
@@ -12,3 +20,5 @@ update_rules(rule_cb=drop_get_orig_source)
 print('Remove unnecessary get-orig-source-target.')
 print('Fixed-Lintian-Tags: '
       'debian-rules-contains-unnecessary-get-orig-source-target')
+if certainty:
+    print('Certainty: %s' % certainty)

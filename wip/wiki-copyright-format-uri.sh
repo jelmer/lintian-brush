21b25d72d4251b063d97ab2d2d73150342bc77a3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2019-12-14 15:10:44 +0000

    Drop wiki-copyright-format-uri; no remaining instances in the archive.

--- wip/wiki-copyright-format-uri.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# Not yet enabled; this should only be done if it doesn't add additional lintian warnings about copyright file.
-perl -p -i -e 's/^(Format|Format-Specification): .*/Format: https:\/\/www.debian.org\/doc\/packaging-manuals\/copyright-format\/1.0\//' debian/copyright
-echo "Use versioned copyright format URI."

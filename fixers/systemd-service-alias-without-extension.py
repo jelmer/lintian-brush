0f7f48fc2cd58cdbd6b7035796b1cee3d27dde02
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-01-01 19:58:59 +0000

    Fix typo.

--- fixers/systemd-service-alias-without-extension.py
+++ fixers/systemd-service-alias-without-extension.py
@@ -7,7 +7,7 @@ for path in systemd_service_files():
     base, required_ext = os.path.splitext(path)
     with SystemdServiceUpdater(path) as updater:
         for i, alias in enumerate(updater.file['Unit']['Alias']):
-            base, ext = os.path.splitext(updater.file['Unit']['Alias'])
+            base, ext = os.path.splitext(alias)
             if ext != required_ext:
                 updater.file['Unit']['Alias'][i] = base + required_ext
 

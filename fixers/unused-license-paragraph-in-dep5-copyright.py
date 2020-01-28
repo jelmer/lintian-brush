b8e6ae40b44fece7e06e243c906e1c30beae3aec
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-01-28 21:49:45 +0000

    Properly parse exceptions in license synopsis.

--- fixers/unused-license-paragraph-in-dep5-copyright.py
+++ fixers/unused-license-paragraph-in-dep5-copyright.py
@@ -1,31 +1,51 @@
 #!/usr/bin/python3
 
 from lintian_brush.copyright import CopyrightUpdater, NotMachineReadableError
+from lintian_brush.fixer import report_result
 import sys
+import re
 
 used = set()
 defined = set()
+certainty = 'certain'
+
+
+def extract_licenses(synopsis):
+    ret = []
+    for license in synopsis.split(" or "):
+        m = re.fullmatch(r'(.*) with (.*) exception', license)
+        if m:
+            license = m.group(1)
+        ret.append(license)
+    return ret
 
 
 try:
     with CopyrightUpdater() as updater:
         if updater.copyright.header.license:
-            if updater.copyright.header.license.synopsis:
-                used.update(
-                    updater.copyright.header.license.synopsis.split(" or "))
             if updater.copyright.header.license.text:
-                defined.update(
-                    updater.copyright.header.license.synopsis.split(" or "))
-        for paragraph in updater.copyright.all_files_paragraphs():
-            if not paragraph.license:
-                continue
-            if paragraph.files:
-                used.update(paragraph.license.synopsis.split(" or "))
+                defined.add(updater.copyright.header.license.synopsis)
         for paragraph in updater.copyright.all_paragraphs():
             if not paragraph.license:
                 continue
             if paragraph.license.text:
-                defined.update(paragraph.license.synopsis.split(" or "))
+                defined.add(paragraph.license.synopsis)
+
+        if updater.copyright.header.license:
+            synopsis = updater.copyright.header.license.synopsis
+            if synopsis:
+                if synopsis in defined:
+                    used.add(synopsis)
+                used.update(extract_licenses(synopsis))
+
+        for paragraph in updater.copyright.all_files_paragraphs():
+            if not paragraph.license:
+                continue
+            if paragraph.files:
+                synopsis = paragraph.license.synopsis
+                if synopsis in defined:
+                    used.add(synopsis)
+                used.update(extract_licenses(synopsis))
 
         extra_defined = (defined - used)
         extra_used = (used - defined)
@@ -33,6 +53,9 @@ try:
         if extra_used:
             sys.stderr.write('Undefined licenses in copyright: %r' %
                              extra_used)
+            # Drop the certainty since it's possible the undefined licenses
+            # are actually the referenced ones.
+            certainty = 'possible'
 
         if extra_defined and not extra_used:
             for paragraph in list(updater.copyright._Copyright__paragraphs):
@@ -43,6 +66,7 @@ try:
 except (FileNotFoundError, NotMachineReadableError):
     pass
 else:
-    print("Remove unused license definitions for %s." %
-          ', '.join(extra_defined))
-    print("Fixed-Lintian-Tags: unused-license-paragraph-in-dep5-copyright")
+    report_result(
+        "Remove unused license definitions for %s." % ', '.join(extra_defined),
+        fixed_lintian_tags=['unused-license-paragraph-in-dep5-copyright'],
+        certainty=certainty)

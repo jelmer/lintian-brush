3f82348675e2bc4bd95fd70448fca48b54cbe5e0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-05-05 20:47:46 +0000

    Add fixer for binary-control-field-duplicates-source.

---
+++ fixers/binary-control-field-duplicates-source.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from lintian_brush.control import ControlUpdater
+from lintian_brush.fixer import report_result
+
+removed = []
+
+with ControlUpdater() as updater:
+    for binary in updater.binaries:
+        for field, value in binary.items():
+            if updater.source.get(field) == value:
+                del binary[field]
+                removed.append((binary['Package'], field, value))
+
+
+report_result(
+    'Remove %s that duplicate%s source.' % (
+        ', '.join(
+            ['%s on %s' % (field, package)
+             for (package, field, value) in removed]),
+        's' if len(removed) == 1 else ''),
+    fixed_lintian_tags=['binary-control-field-duplicates-source'])

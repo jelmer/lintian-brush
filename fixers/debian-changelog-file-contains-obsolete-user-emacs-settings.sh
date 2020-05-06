7f0038e14fe25280ea91493a8200ea50f2aeaafa
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-05-06 20:08:45 +0000

    debian-changelog-file-contains-obsolete-user-emacs-settings: Only remove add-log-mailing-address, not mode lines.

--- fixers/debian-changelog-file-contains-obsolete-user-emacs-settings.sh
+++ fixers/debian-changelog-file-contains-obsolete-user-emacs-settings.sh
@@ -3,14 +3,8 @@
 perl -pe 'BEGIN{undef $/;} s/(Local variables:.*?)
 add-log-mailing-address: .*
 (End:)
-/
-\1
+/\1
 \2
-/smg' -i debian/changelog
-
-perl -pe 'BEGIN{undef $/;} s/([\n]*Local variables:.*?)
-mode: debian-changelog
-(End:[\n]+)/
 /smg' -i debian/changelog
 
 echo "Drop no longer supported add-log-mailing-address setting from debian/changelog."

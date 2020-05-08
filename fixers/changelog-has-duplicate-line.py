65395b1d099d997cbd5808fb689ecfe3cf860402
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-05-08 12:59:16 +0000

    Track where we last saw a particular line.

--- fixers/changelog-has-duplicate-line.py
+++ fixers/changelog-has-duplicate-line.py
@@ -6,13 +6,14 @@ from lintian_brush.fixer import report_result
 with ChangelogUpdater() as updater:
     block = updater.changelog[0]
     if block.distributions == 'UNRELEASED':
-        seen = set()
+        seen = {}
         for (author, linenos, change) in changes_by_author(block.changes()):
             change = ''.join(change)
             if (author, change) in seen:
                 for lineno in reversed(linenos):
                     del block._changes[lineno]
-            seen.add((author, change))
+            else:
+                seen[(author, change)] = linenos
 
 
 report_result('Remove duplicate line from changelog.')

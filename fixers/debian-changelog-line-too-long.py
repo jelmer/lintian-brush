00fe9df74bbfceb80551682840a78b9cee3270c0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-05-26 11:51:18 +0000

    debian-changelog-line-too-long: Only claim lintian tags were fixed when fixing long lines in the last changelog entry.

--- fixers/debian-changelog-line-too-long.py
+++ fixers/debian-changelog-line-too-long.py
@@ -7,10 +7,12 @@ from lintian_brush.changelog import (
     )
 from lintian_brush.fixer import report_result
 
+fixed_lintian_tags = set()
+
 
 updated = []
 with ChangelogUpdater() as updater:
-    for block in updater.changelog:
+    for i, block in enumerate(updater.changelog):
         if not any_long_lines(block.changes()):
             continue
         old_changes = list(block._changes)
@@ -18,8 +20,11 @@ with ChangelogUpdater() as updater:
         if old_changes != new_changes:
             block._changes = new_changes
             updated.append(block.version)
+            if i == 0:
+                # Lintian only warns about the first block.
+                fixed_lintian_tags.add('debian-changelog-line-too-long')
 
 report_result(
     'Wrap long lines in changelog entries: %s.' % (
      ', '.join([str(v) for v in updated])),
-    fixed_lintian_tags=['debian-changelog-line-too-long'])
+    fixed_lintian_tags=fixed_lintian_tags)

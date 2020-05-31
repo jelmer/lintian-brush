f14eafa4b44a23e03d195aa380c4bb8c934b073f
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-05-31 02:21:52 +0000

    systemd-service-file-pidfile-refers-to-var-run: Also update other references to pid file.

--- fixers/systemd-service-file-pidfile-refers-to-var-run.py
+++ fixers/systemd-service-file-pidfile-refers-to-var-run.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 
+from lintian_brush.fixer import report_result
 from lintian_brush.systemd import systemd_service_files, SystemdServiceUpdater
 
 
 for path in systemd_service_files():
     with SystemdServiceUpdater(path) as updater:
-        val = updater.file['Service']['PIDFile']
-        if isinstance(val, str):
-            updater.file['Service']['PIDFile'] = val.replace(
-                "/var/run/", "/run/")
+        old_pidfile = updater.file['Service']['PIDFile']
+        if isinstance(old_pidfile, str):
+            new_pidfile = old_pidfile.replace("/var/run/", "/run/")
+            updater.file['Service']['PIDFile'] = new_pidfile
+        for key in updater.file['Service']:
+            val = updater.file['Service'][key]
+            if old_pidfile not in val:
+                continue
+            updater.file['Service'][key] = val.replace(
+                old_pidfile, new_pidfile)
 
-
-print("Replace /var/run with /run for the Service PIDFile.")
-print("Fixed-Lintian-Tags: systemd-service-file-pidfile-refers-to-var-run")
+report_result(
+    "Replace /var/run with /run for the Service PIDFile.",
+    fixed_lintian_tags=['systemd-service-file-pidfile-refers-to-var-run'])

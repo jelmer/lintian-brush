4c99750c195763e977c792353f50d511141ac927
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-06-08 18:31:40 +0000

    Don't just reformat Restrictions field if there are no changes.

--- fixers/obsolete-runtime-tests-restriction.py
+++ fixers/obsolete-runtime-tests-restriction.py
@@ -4,37 +4,62 @@ import os
 import sys
 
 from lintian_brush.deb822 import Deb822Updater
+from lintian_brush.control import delete_from_list
+from lintian_brush.fixer import report_result
 
 
-DEPRECATED_RESTRICTIONS = ['needs-recommends']
 removed_restrictions = []
 
 
 if not os.path.exists('debian/tests/control'):
     sys.exit(0)
 
+KNOWN_OBSOLETE_RESTRICTIONS_PATH = (
+    '/usr/share/lintian/data/testsuite/known-obsolete-restrictions')
+DEPRECATED_RESTRICTIONS = []
+
+try:
+    with open(KNOWN_OBSOLETE_RESTRICTIONS_PATH, 'r') as f:
+        for line in f:
+            if line.startswith('#'):
+                continue
+            if not line.strip():
+                continue
+            DEPRECATED_RESTRICTIONS.append(line.strip())
+except FileNotFoundError:
+    sys.exit(2)
+
 with Deb822Updater('debian/tests/control') as updater:
     for paragraph in updater.paragraphs:
         restrictions = paragraph.get('Restrictions', '').split(',')
         if restrictions == ['']:
             continue
+        to_delete = []
         for i, restriction in enumerate(list(restrictions)):
             if restriction.strip() in DEPRECATED_RESTRICTIONS:
-                del restrictions[i]
-                removed_restrictions.append(restriction.strip())
-        paragraph['Restrictions'] = ','.join(restrictions).lstrip()
-        if not paragraph['Restrictions'].strip():
-            del paragraph['Restrictions']
+                to_delete.append(restriction.strip())
+        if to_delete:
+            removed_restrictions.extend(to_delete)
+            paragraph['Restrictions'] = delete_from_list(
+                paragraph['Restrictions'], to_delete)
+            if not paragraph['Restrictions'].strip():
+                del paragraph['Restrictions']
 
 
-print('Drop deprecated restriction%s %s. See '
-      'https://salsa.debian.org/ci-team/autopkgtest/tree/'
-      'master/doc/README.package-tests.rst' % (
-       's' if len(removed_restrictions) > 1 else '',
-       ', ' .join(removed_restrictions)))
-print('Fixed-Lintian-Tags: obsolete-runtime-tests-restriction')
 if 'needs-recommends' in removed_restrictions:
     # This is Certainty: possible, since the package may actually rely on the
     # (direct? indirect?) recommends, in which case we'd want to add them to
     # Depends.
-    print('Certainty: possible')
+    certainty = 'possible'
+else:
+    certainty = 'certain'
+
+
+report_result(
+    'Drop deprecated restriction%s %s. See '
+    'https://salsa.debian.org/ci-team/autopkgtest/tree/'
+    'master/doc/README.package-tests.rst' % (
+       's' if len(removed_restrictions) > 1 else '',
+       ', ' .join(removed_restrictions)),
+    fixed_lintian_tags=['obsolete-runtime-tests-restriction'],
+    certainty=certainty)

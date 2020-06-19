885d7133ddeb4b7f8c1891bcb495417ae45a0d73
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-06-19 16:51:35 +0000

    Add tag status script.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 VERSION=$(shell dpkg-parsechangelog | grep Version: | cut -d " " -f 2)
 
-check:: style testsuite
+check:: style testsuite tag-status
 
 FIXERS = $(patsubst fixers/%.sh,%,$(wildcard fixers/*.sh)) $(patsubst fixers/%.py,%,$(wildcard fixers/*.py))
 
@@ -15,16 +15,29 @@ style::
 typing::
 	mypy lintian_brush fixers
 
+tag-status::
+	python3 tag-status.py --check
+
 testsuite::
 	python3 setup.py test
 
 README.md::
 	PYTHONPATH=$(PWD) ./buildtools/update-readme.py
 
-unsupported:
+lintian-tags:
 	lintian-info --list-tags > lintian-tags
+
+.PHONY: lintian-tags lintian-brush-tags
+
+lintian-brush-tags:
 	PYTHONPATH=$(PWD) python3 -m lintian_brush --list-tags 2> lintian-brush-tags
-	awk 'NR==FNR{a[$$0]=1;next}!a[$$0]' lintian-brush-tags lintian-tags
+
+unsupported: lintian-tags lintian-brush-tags
+	awk 'NR==FNR{a[$$0]=1;next}!a[$$0]' lintian-brush-tags lintian-tags $@
+
+candidates: lintian-tags lintian-brush-tags
+	awk 'NR==FNR{a[$$0]=1;next}!a[$$0]' lintian-brush-tags lintian-tags > unsupported
+	awk 'NR==FNR{a[$$0]=1;next}!a[$$0]' hard unsupported
 
 update-readme:
 	brz diff README.md
---
+++ tag-status.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import argparse
+import subprocess
+import sys
+
+from lintian_brush import available_lintian_fixers
+
+from ruamel.yaml import YAML
+
+KNOWN_KEYS = ['tag', 'status', 'difficulty', 'comment']
+
+all_tags = set([
+    tag.decode()
+    for tag in
+    subprocess.check_output(
+        ['lintian-info', '--list-tags']).splitlines()])
+
+supported_tags = set()
+for fixer in available_lintian_fixers():
+    supported_tags.update(fixer.lintian_tags)
+
+yaml = YAML()
+with open('tag-status.yaml', 'r') as f:
+    tag_status = yaml.load(f)
+
+per_tag_status = {}
+for entry in tag_status or []:
+    per_tag_status[entry['tag']] = entry
+    extra_keys = set(entry.keys()) - set(KNOWN_KEYS)
+    assert not extra_keys, 'Unknown keys: %r' % extra_keys
+
+
+for tag in supported_tags:
+    existing = per_tag_status.get(tag)
+    if existing and existing['status'] != 'implemented':
+        raise Exception(
+            'tag %s is marked as %s in tag-status.yaml, but implemented' %
+            (tag, existing['status']))
+    per_tag_status[tag] = {'status': 'implemented'}
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    '--new-tags', action='store_true', help='List missing tags.')
+parser.add_argument(
+    '--check', action='store_true', help='Check tags.')
+args = parser.parse_args()
+
+if args.new_tags:
+    for tag in sorted(all_tags):
+        if tag not in per_tag_status:
+            print(tag)
+elif args.check:
+    retcode = 0
+    for tag in sorted(all_tags):
+        if tag not in per_tag_status:
+            print(tag)
+            retcode = 1
+    sys.exit(retcode)

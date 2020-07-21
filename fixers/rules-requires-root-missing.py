93f1d07bcbfdc4db3a3db65d9f5e56da561d6934
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-07-21 00:41:48 +0000

    Add test for rules-requires-root and simplify fixer.

--- fixers/rules-requires-root-missing.py
+++ fixers/rules-requires-root-missing.py
@@ -1,23 +1,22 @@
 #!/usr/bin/python3
 
-from debmutate.control import ( ControlEditor )
-from lintian_brush.fixer import report_result
+from debmutate.control import ControlEditor
+from lintian_brush.fixer import report_result, meets_minimum_certainty
+import sys
 
 require_root = "no"
-updated = False
+CERTAINTY = "possible"
+
+if not meets_minimum_certainty(CERTAINTY):
+    sys.exit(0)
 
 with ControlEditor() as updater:
     if "Rules-Requires-Root" not in updater.source:
-        # XXX add some heuristics to set require_root = "yes" in common
+        # TODO: add some heuristics to set require_root = "yes" in common
         # cases, like `debian/rules binary` chown(1)'ing stuff
         updater.source["Rules-Requires-Root"] = require_root
-        updated = True
 
 report_result(
     "Set Rules-Requires-Root: %s." % require_root,
-    fixed_lintian_tags=["rules-requires-root-missing"])
-
-if require_root == "no" and updated == True:
-    with open("/dev/tty", "w") as p:
-      # ugly work around lintian-brush capturing the error output 
-      p.write("NOTE: Don't forget to verify that the binaries built with this field present are identical!\n")
+    fixed_lintian_tags=["rules-requires-root-missing"],
+    certainty=CERTAINTY)

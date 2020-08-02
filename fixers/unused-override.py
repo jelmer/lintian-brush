e39b22deb2cd1b04e1e8bcc77ab8177e83ebe13b
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-08-02 02:59:39 +0000

    Only remove overrides if asyncpg is available to contact UDD. fixers/unused-override.py

--- fixers/unused-override.py
+++ fixers/unused-override.py
@@ -3,8 +3,13 @@
 import os
 import sys
 
+try:
+    import asyncpg  # noqa: F401
+except ModuleNotFoundError:
+    sys.exit(2)
+
 from lintian_brush.lintian_overrides import remove_unused
-from lintian_brush.fixer import net_access_allowed
+from lintian_brush.fixer import net_access_allowed, report_result
 
 if int(os.environ.get('DILIGENCE', '0')) < 1:
     # Removing unused overrides requires pro-actively contacting UDD.
@@ -15,9 +20,14 @@ if not net_access_allowed():
 
 removed = remove_unused()
 
-print('Remove %d unused lintian overrides.' % len(removed))
-print('')
+description = [
+    'Remove %d unused lintian overrides.\n' % len(removed),
+    '\n',
+    ]
 for override in removed:
-    print('* %s' % override.tag)
-print('Fixed-Lintian-Tags: unused-override')
-print('Certainty: certain')
+    description.append('* %s\n' % override.tag)
+
+report_result(
+    ''.join(description),
+    fixed_lintian_tags=['unused-override'],
+    certainty='certain')

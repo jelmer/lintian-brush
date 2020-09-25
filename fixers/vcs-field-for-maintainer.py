97247146b6b9f4b9634f7e050c7382b35712807e
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-09-25 13:34:11 +0000

    Add fixers for old-dpmt-vcs and old-papt-vcs.

---
+++ fixers/vcs-field-for-maintainer.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+from debmutate.control import ControlEditor
+from email.utils import parseaddr
+from lintian_brush.fixer import report_result, fixed_lintian_tag
+
+REPLACEMENTS = {
+    'python-modules-team@lists.alioth.debian.org':
+        ('old-dpmt-vcs',
+         [('https://salsa.debian.org/python-team/modules/',
+           'https://salsa.debian.org/python-team/packages/')]),
+    'python-apps-team@lists.alioth.debian.org':
+        ('old-papt-vcs',
+         [('https://salsa.debian.org/python-team/applications/',
+           'https://salsa.debian.org/python-team/packages/')]),
+}
+
+with ControlEditor() as editor:
+    maint, email = parseaddr(editor.source['Maintainer'])
+    changed_fields = set()
+    try:
+        tag, replacements = REPLACEMENTS[email]
+    except KeyError:
+        pass
+    else:
+        for field in [f for f in editor.source if f.startswith('Vcs-')]:
+            url = editor.source[field]
+            for old, new in replacements:
+                newurl = url.replace(old, new)
+                if newurl != url:
+                    editor.source[field] = newurl
+                    changed_fields.add(field)
+                    fixed_lintian_tag(editor.source, tag, info='')
+                url = newurl
+
+    report_result('Update fields %s for maintainer %s.' % (
+                  ', '.join(sorted(changed_fields)), maint))

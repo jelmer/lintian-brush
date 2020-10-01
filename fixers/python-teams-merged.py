c1e20ba14da173fa963bae85d3121c3546b7b9bf
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-10-01 23:01:15 +0000

    Move tests, report correct info, update changelog.

--- fixers/python-teams-merged.py
+++ fixers/python-teams-merged.py
@@ -5,12 +5,14 @@ from email.utils import parseaddr
 from lintian_brush.fixer import report_result, fixed_lintian_tag
 
 with ControlEditor() as editor:
-    maint, email = parseaddr(editor.source['Maintainer'])
+    old_maintainer = editor.source['Maintainer']
+    maint, email = parseaddr(old_maintainer)
     if email in (
             'python-modules-team@lists.alioth.debian.org',
             'python-apps-team@lists.alioth.debian.org'):
         editor.source['Maintainer'] = (
             'Debian Python Team <team+python@tracker.debian.org>')
-        fixed_lintian_tag(editor.source, 'python-teams-merged', info=())
+        fixed_lintian_tag(
+            editor.source, 'python-teams-merged', info=(old_maintainer, ))
 
 report_result('Update maintainer email for merge of DPMT and PAPT.')

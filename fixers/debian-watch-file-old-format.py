3df5cfed61b9f78e2c4b0f9e1b163f369e6f4b73
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-11-05 18:50:53 +0000

    Add tags added in lintian 05ea5176216d2f05704481f8d3467bcd86d8d0e9.

--- fixers/debian-watch-file-old-format.py
+++ fixers/debian-watch-file-old-format.py
@@ -2,14 +2,24 @@
 
 from debmutate.watch import WatchEditor
 
-from lintian_brush.fixer import report_result
+from lintian_brush.fixer import report_result, LintianIssue
 
+OBSOLETE_WATCH_FILE_FORMAT = 2
 WATCH_FILE_LATEST_VERSION = 4
 
 
 with WatchEditor() as editor:
-    if editor.watch_file.version != 4:
-        editor.watch_file.version = 4
+    if editor.watch_file.version >= WATCH_FILE_LATEST_VERSION:
+        pass
+    else:
+        if editor.watch_file.version <= OBSOLETE_WATCH_FILE_FORMAT:
+            tag = 'obsolete-debian-watch-file-standard'
+        else:
+            tag = 'older-debian-watch-file-standard'
+        issue = LintianIssue('source', tag, '%d' % editor.watch_file.version)
+        if issue.should_fix():
+            editor.watch_file.version = WATCH_FILE_LATEST_VERSION
+            issue.report_fixed()
 
 
 report_result(

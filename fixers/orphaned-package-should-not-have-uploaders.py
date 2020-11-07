756bf773e15d540ea26110470be903c0029d5fde
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-11-07 23:10:17 +0000

    Use new API.

--- fixers/orphaned-package-should-not-have-uploaders.py
+++ fixers/orphaned-package-should-not-have-uploaders.py
@@ -3,19 +3,18 @@
 from email.utils import parseaddr
 from debmutate.control import ControlEditor
 
-from lintian_brush.fixer import report_result, fixed_lintian_tag
+from lintian_brush.fixer import report_result, LintianIssue
 
 
 with ControlEditor() as updater:
     if ("Maintainer" in updater.source and
             parseaddr(updater.source["Maintainer"])[1] ==
-            'packages@qa.debian.org'):
-        try:
+            'packages@qa.debian.org' and 'Uploaders' in updater.source):
+        issue = LintianIssue(
+            updater.source, 'uploaders-in-orphan', info=())
+        if issue.should_fix():
             del updater.source["Uploaders"]
-        except KeyError:
-            pass
-        else:
-            fixed_lintian_tag(updater.source, 'uploaders-in-orphan')
+            issue.report_fixed()
 
 
 report_result("Remove uploaders from orphaned package.")

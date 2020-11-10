c2497d3e645ef821d30fb336fb875aea191e1013
Jelmer Vernooĳ <jelmer@jelmer.uk>
2020-11-10 19:27:01 +0000

    Simplify the code a little bit.

--- fixers/autotools-pkg-config-macro-not-cross-compilation-safe.py
+++ fixers/autotools-pkg-config-macro-not-cross-compilation-safe.py
@@ -5,56 +5,67 @@ from lintian_brush.fixer import (
     report_result,
     LintianIssue,
     )
+import os
 import re
 from typing import List
 
 resolution = ""
 
 
-for name in ['configure.ac', 'configure.in']:
+def update_configure_ac(path):
+    global resolution
+    if not os.path.exists(path):
+        return
     oldlines: List[bytes] = []
     newlines: List[bytes] = []
 
-    try:
-        with open(name, 'rb') as f:
-            for lineno, line in enumerate(f, 1):
-                m = re.fullmatch(
-                    b'\\s*AC_PATH_PROG\\s*'
-                    b'\\(\\s*(\\[)?(?P<variable>[A-Z_]+)(\\])?\\s*'
-                    b',\\s*(\\[)?pkg-config(\\])?\\s*'
-                    b'(,\\s*(\\[)?(?P<default>.*)(\\])?\\s*)?\\)\n', line)
-                if m:
-                    issue = LintianIssue(
-                        'source',
-                        'autotools-pkg-config-macro-not-cross-compilation-'
-                        'safe', info='%s (line %d)' % (name, lineno))
-                    # TODO(jelmer): Check overrides
-                    issue.report_fixed()
-                    if (m.group('variable') == b'PKG_CONFIG' and
-                            not m.group('default')):
-                        newlines.append(b'PKG_PROG_PKG_CONFIG\n')
-                        resolution = (
-                            "This patch changes it to use "
-                            "PKG_PROG_PKG_CONFIG macro from pkg.m4.")
-                        # Build-Depend on pkg-config for pkg.m4
-                        with ControlEditor() as control:
-                            control.source['Build-Depends'] = (
-                                ensure_some_version(
-                                    control.source.get('Build-Depends', ''),
-                                    'pkg-config'))
-                    else:
-                        newlines.append(
-                            line.replace(b'AC_PATH_PROG', b'AC_PATH_TOOL'))
-                        resolution = (
-                            "This patch changes it to use AC_PATH_TOOL.")
+    with open(path, 'rb') as f:
+        for lineno, line in enumerate(f, 1):
+            m = re.fullmatch(
+                b'\\s*AC_PATH_PROG\\s*'
+                b'\\(\\s*(\\[)?(?P<variable>[A-Z_]+)(\\])?\\s*'
+                b',\\s*(\\[)?pkg-config(\\])?\\s*'
+                b'(,\\s*(\\[)?(?P<default>.*)(\\])?\\s*)?\\)\n', line)
+
+            if not m:
+                newlines.append(line)
+                continue
+
+            if m:
+                issue = LintianIssue(
+                    'source',
+                    'autotools-pkg-config-macro-not-cross-compilation-'
+                    'safe', info='%s (line %d)' % (name, lineno))
+                if not issue.should_fix():
+                    newlines.append(line)
                     continue
 
-                newlines.append(line)
-    except FileNotFoundError:
-        continue
-    if oldlines != newlines:
-        with open(name, 'wb') as f:
-            f.writelines(newlines)
+            issue.report_fixed()
+            if (m.group('variable') == b'PKG_CONFIG' and
+                    not m.group('default')):
+                newlines.append(b'PKG_PROG_PKG_CONFIG\n')
+                resolution = (
+                    "This patch changes it to use "
+                    "PKG_PROG_PKG_CONFIG macro from pkg.m4.")
+                # Build-Depend on pkg-config for pkg.m4
+                with ControlEditor() as control:
+                    control.source['Build-Depends'] = (
+                        ensure_some_version(
+                            control.source.get('Build-Depends', ''),
+                            'pkg-config'))
+            else:
+                newlines.append(
+                    line.replace(b'AC_PATH_PROG', b'AC_PATH_TOOL'))
+                resolution = (
+                    "This patch changes it to use AC_PATH_TOOL.")
+    if oldlines == newlines:
+        return
+    with open(path, 'wb') as f:
+        f.writelines(newlines)
+
+
+for name in ['configure.ac', 'configure.in']:
+    update_configure_ac(name)
 
 
 report_result(

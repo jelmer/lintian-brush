584b6e819c1194371b0fd6dca6c14102ceaca8b5
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-02-24 13:01:05 +0000

    Drop wip directory.

--- wip/newer-standards-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# TODO(jelmer): Find the latest version of debian-policy to use here, instead of hardcoding 4.2.1
-perl -p -i -e 's/^Standards-Version: .*/Standards-Version: 4.2.1/' debian/control
-echo "Use most recent version in Standards-Version field rather than non-existent future version."

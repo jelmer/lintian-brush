332d7466dced1e3d9a8c74e78040553b45652921
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-13 15:05:05 +0000

    Add fixer for extended-description-contains-empty-paragraph.

---
+++ fixers/extended-description-contains-empty-paragraph.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from lintian_brush.fixer import report_result, LintianIssue, control
+
+with control as editor:
+    for binary in control.binaries:
+        try:
+            desc = binary['Description']
+        except KeyError:
+            # Uhm, okay.
+            continue
+        lines = desc.splitlines(True)
+        issue = LintianIssue(
+            binary, 'extended-description-contains-empty-paragraph', ())
+        if not issue.should_fix():
+            continue
+        if lines[1] == ' .\n':
+            del lines[1]
+        binary['Description'] = ''.join(lines)
+        issue.report_fixed()
+
+report_result('Remove empty leading paragraph in Description.')

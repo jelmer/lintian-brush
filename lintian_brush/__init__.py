f83ebdbf3b55728f29be751b0d247cd4a21612c1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-03-29 18:56:25 +0100

    Bump version.

--- lintian_brush/__init__.py
+++ lintian_brush/__init__.py
@@ -52,7 +52,7 @@ from debian.deb822 import Deb822
 from debmutate.reformatting import FormattingUnpreservable
 
 
-__version__ = (0, 102)
+__version__ = (0, 104)
 version_string = ".".join(map(str, __version__))
 SUPPORTED_CERTAINTIES = ["certain", "confident", "likely", "possible", None]
 DEFAULT_MINIMUM_CERTAINTY = "certain"

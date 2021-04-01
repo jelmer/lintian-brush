d0f29c872f5a1ccdf143041b48a399edb37ada47
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-04-01 14:09:53 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup
 
 setup(
     name="lintian-brush",
-    version="0.104",
+    version="0.105",
     author="Jelmer Vernooij",
     author_email="jelmer@debian.org",
     packages=["lintian_brush"],

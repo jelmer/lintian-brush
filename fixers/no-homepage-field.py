d05bb15d26092979fa5828cbc41506f7bf2a95b0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-04-09 00:34:47 +0100

    Add fixer for pypi-homepage.

--- fixers/no-homepage-field.py
+++ fixers/no-homepage-field.py
@@ -1,30 +1,42 @@
 #!/usr/bin/python3
 
+from urllib.parse import urlparse
+
 from lintian_brush.fixer import (
     control,
-    fixed_lintian_tag,
     meets_minimum_certainty,
     report_result,
     trust_package,
+    LintianIssue,
     )
-from upstream_ontologist.guess import guess_upstream_metadata_items
+from upstream_ontologist.guess import guess_upstream_metadata_items, known_bad_guess
 
-current_certainty = None
 
-# TODO(jelmer): Support editing homepage field in debian/debcargo.toml
+def guess_homepage():
+    for datum in guess_upstream_metadata_items(
+            '.', trust_package=trust_package()):
+        if datum.field != 'Homepage':
+            continue
+        if not meets_minimum_certainty(datum.certainty):
+            continue
+        if known_bad_guess(datum):
+            continue
+        return datum
+
 
 with control as updater:
     if 'Homepage' not in updater.source:
-        for datum in guess_upstream_metadata_items(
-                '.', trust_package=trust_package()):
-            if datum.field != 'Homepage':
-                continue
-            if not meets_minimum_certainty(datum.certainty):
-                continue
-            if current_certainty != 'certain':
+        datum = guess_homepage()
+        issue = LintianIssue('source', 'no-homepage-field')
+        if datum and issue.should_fix():
+            updater.source["Homepage"] = datum.value
+            issue.report_fixed()
+            report_result('Fill in Homepage field.', certainty=datum.certainty)
+    else:
+        if urlparse(updater.source['Homepage']).hostname == 'pypi.org':
+            issue = LintianIssue('source', 'pypi-homepage', updater.source['Homepage'])
+            datum = guess_homepage()
+            if issue.should_fix() and datum:
                 updater.source["Homepage"] = datum.value
-                current_certainty = datum.certainty
-                fixed_lintian_tag('source', 'no-homepage-field')
-
-
-report_result('Fill in Homepage field.', certainty=current_certainty)
+                issue.report_fixed()
+                report_result('Avoid pypi in Homepage field.', certainty=datum.certainty)

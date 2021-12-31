483c377e67a10bda7affaf80da4f7e096524b7f0
Jelmer Vernooĳ <jelmer@jelmer.uk>
2021-12-31 22:05:15 +0000

    Import ben support from debmutate.

--- lintian_brush/transition_apply.py
+++ lintian_brush/transition_apply.py
@@ -25,6 +25,7 @@ import re
 
 from breezy.workingtree import WorkingTree
 
+from debmutate.ben import parse_ben, SUPPORTED_KEYS
 from debmutate.control import ControlEditor
 from debmutate.deb822 import ChangeConflict
 from debmutate.reformatting import (
@@ -60,63 +61,6 @@ def _note_changelog_policy(policy, msg):
             extra = "Specify --update-changelog to override."
         logging.info("%s %s", msg, extra)
     _changelog_policy_noted = True
-
-
-def _parse_benitem(v):
-    if v == 'false':
-        return False
-    elif v == 'true':
-        return True
-    elif v.startswith('[') and v.endswith(']'):
-        return [_parse_benitem(i) for i in v.split(';')]
-    elif v.startswith('"') and v.endswith('"'):
-        return v[1:-1]
-    elif v.isdigit():
-        return int(v)
-    elif '~' in v:
-        regex = []
-        for o in v.split(' | '):
-            try:
-                field, expr = o.split('~', 1)
-            except ValueError:
-                raise ValueError('expected ~: %r' % o)
-            expr = re.compile(_parse_benitem(expr.strip()))
-            regex.append((field.strip(), expr))
-        return regex
-    else:
-        return v
-
-
-def parse_ben(f):
-    ret = {}
-    assignment = {}
-    lastk = None
-    v = ''
-    for line in f:
-        if lastk is None:
-            if not line.strip():
-                continue
-            if line.startswith('#'):
-                continue
-            line = line.rstrip()
-            (k, v) = line.split('=', 1)
-            k = k.strip()
-            v = v.lstrip()
-            lastk = k
-        else:
-            v += line
-        if v.rstrip().endswith(';'):
-            assignment[lastk] = v[:-1]
-            lastk = None
-    if lastk is not None:
-        raise ValueError('unterminated key: %r' % lastk)
-    for k, v in assignment.items():
-        ret[k] = _parse_benitem(v)
-    return ret
-
-
-SUPPORTED_KEYS = [
-    'title', 'notes', 'is_affected', 'is_good', 'is_bad', 'export']
 
 
 def control_matches(control, ors):

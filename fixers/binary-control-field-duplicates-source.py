03539c83e3698ae8cbff8e8357a00ee7be40e2c2
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-02-19 22:42:21 +0000

    Make changelog messages for binary-control-field-duplicates-source shorter.

--- fixers/binary-control-field-duplicates-source.py
+++ fixers/binary-control-field-duplicates-source.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 
 import sys
+from typing import Dict, Set
 
 from lintian_brush.fixer import control, report_result, LintianIssue
 
-removed = []
+removed: Dict[str, Set[str]] = {}
 
 try:
     with control as updater:
         for binary in updater.binaries:
-            for field, value in binary.items():
+            for field, value in list(binary.items()):
                 if updater.source.get(field) == value:
                     del binary[field]
-                    removed.append((binary['Package'], field, value))
+                    removed.setdefault(field, set()).add(binary['Package'])
                     issue = LintianIssue(
                         updater.source,
                         'installable-field-mirrors-source',
@@ -23,9 +24,14 @@ except FileNotFoundError:
     sys.exit(0)
 
 
-report_result(
-    'Remove %s that duplicate%s source.' % (
-        ', '.join(
-            ['%s on %s' % (field, package)
-             for (package, field, value) in removed]),
-        's' if len(removed) == 1 else ''))
+if len(removed) == 1:
+    (field, binary_packages) = list(removed.items())[0]
+    report_result(
+        'Remove field %s on binary package%s %s that duplicates source.' % (
+            field, 's' if len(binary_packages) != 1 else '',
+            ', '.join(sorted(binary_packages))))
+elif len(removed) > 1:
+    report_result(
+            'Remove fields on binary packages that duplicate source:\n' +
+            '\n'.join(['+ Field %s from %s.' % (field, ', '.join(sorted(packages)))
+                       for (field, packages) in sorted(removed.items())]))

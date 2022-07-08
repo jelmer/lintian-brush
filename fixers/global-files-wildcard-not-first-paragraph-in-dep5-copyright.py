99deddca4b2c8d2b1f5d0077c5b1d64ebe5fa320
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-07-08 14:23:52 +0100

    Avoid unnecessary indirection.

--- fixers/global-files-wildcard-not-first-paragraph-in-dep5-copyright.py
+++ fixers/global-files-wildcard-not-first-paragraph-in-dep5-copyright.py
@@ -5,25 +5,21 @@ from debmutate.copyright import CopyrightEditor, NotMachineReadableError
 from lintian_brush.fixer import report_result, LintianIssue
 
 
-def swap_files_glob(copyright):
-    files_i = 0
-    for i, paragraph in enumerate(copyright._Copyright__paragraphs):
-        if "Files" in paragraph:
-            if paragraph["Files"] == "*" and files_i > 0:
-                issue = LintianIssue(
-                    'source',
-                    'global-files-wildcard-not-first-paragraph-in-'
-                    'dep5-copyright')
-                if issue.should_fix():
-                    copyright._Copyright__paragraphs.insert(
-                        0, copyright._Copyright__paragraphs.pop(i))
-                    issue.report_fixed()
-            files_i += 1
-
-
 try:
-    with CopyrightEditor() as updater:
-        swap_files_glob(updater.copyright)
+    with CopyrightEditor() as editor:
+        files_i = 0
+        for i, paragraph in enumerate(editor.copyright._Copyright__paragraphs):
+            if "Files" in paragraph:
+                if paragraph["Files"] == "*" and files_i > 0:
+                    issue = LintianIssue(
+                        'source',
+                        'global-files-wildcard-not-first-paragraph-in-'
+                        'dep5-copyright')
+                    if issue.should_fix():
+                        editor.copyright._Copyright__paragraphs.insert(
+                            0, editor.copyright._Copyright__paragraphs.pop(i))
+                        issue.report_fixed()
+                files_i += 1
 except (FileNotFoundError, NotMachineReadableError):
     pass
 

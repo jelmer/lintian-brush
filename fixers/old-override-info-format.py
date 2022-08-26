bc5bee375a22a716d30cd05d80809abed4771d47
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-08-26 13:29:29 +0100

    More regexes.

--- fixers/old-override-info-format.py
+++ fixers/old-override-info-format.py
@@ -9,16 +9,51 @@ from lintian_brush.lintian_overrides import (
     load_renamed_tags,
     )
 
+LINENO_MATCH = r"\d+|\*"
+
 # Simple match of "$file (line $lineno)"
 PURE_FLN_SUB = (
-    r"^(?P<path>.+) \(line (?P<lineno>\d+)\)$", r"[\1:\2]")
+    r"^(?P<path>.+) \(line (?P<lineno>" + LINENO_MATCH + r")\)$", r"[\1:\2]")
 PURE_FLN_WILDCARD_SUB = (
-    r"^(?P<path>.+) \(line (?P<lineno>\d+)\)$", r"* [\1:\2]")
+    r"^(?P<path>.+) \(line (?P<lineno>" + LINENO_MATCH + r")\)$", r"* [\1:\2]")
 
 
 INFO_FIXERS = {
-    "autotools-pkg-config-macro-not-cross-compilation-safe": PURE_FLN_WILDCARD_SUB,
+    "autotools-pkg-config-macro-not-cross-compilation-safe":
+        PURE_FLN_WILDCARD_SUB,
     "debian-rules-parses-dpkg-parsechangelog": PURE_FLN_SUB,
+    "debian-rules-should-not-use-custom-compression-settings": 
+        ("(.*) \(line (" + LINENO_MATCH + ")\)", r"\1 [debian/rules:\2]"),
+    "debian-source-options-has-custom-compression-settings":
+        ("(.*) \(line (" + LINENO_MATCH + ")\)",
+            r"\1 [debian/source/options:\2]"),
+    "global-files-wildcard-not-first-paragraph-in-dep5-copyright":
+        PURE_FLN_SUB,
+    "missing-license-paragraph-in-dep5-copyright": (
+        r"([^ ]+) (.*) \(line (" + LINENO_MATCH + ")\)",
+        r"\2 [\1:\3]"),
+    "unused-license-paragraph-in-dep5-copyright": (
+        r"([^ ]+) (.*) \(line (" + LINENO_MATCH + ")\)",
+        r"\2 [\1:\3]"),
+    "license-problem-undefined-license": (
+        r"(.*) \(line (" + LINENO_MATCH + r")\)", r"\1 [debian/copyright:\2]"),
+    "debhelper-tools-from-autotools-dev-are-deprecated": (
+        r"(.*) \(line (" + LINENO_MATCH + r")\)", r"\1 [debian/rules:\2]"),
+    "version-substvar-for-external-package": (
+        r"([^ ]+) \(line (" + LINENO_MATCH + r")\) (.*)",
+        r"\1 \3 [debian/control:\2]"),
+    "debian-watch-uses-insecure-uri": (
+        r"(.*) \(line (" + LINENO_MATCH + r")\)", r"\1 [debian/watch:\2]"),
+    "uses-deprecated-adttmp": (
+        r"([^ ]+) \(line (" + LINENO_MATCH + r")\)", r"[\1:\2]"),
+    "incomplete-creative-commons-license": (
+        r"(.*) \(line (" + LINENO_MATCH + r")\)", r"\1 [debian/copyright:\2]"),
+    "debian-rules-sets-dpkg-architecture-variable": (
+        r"(.*) \(line (" + LINENO_MATCH + r")\)", r"\1 [debian/rules:\2]"),
+    "override_dh_auto_test-does-not-check-DEB_BUILD_OPTIONS": (
+        r"(.*) \(line (" + LINENO_MATCH + r")\)", r"\1 [debian/rules:\2]"),
+    "dh-quilt-addon-but-quilt-source-format": (
+        r"(.*) \(line (" + LINENO_MATCH + r")\)", r"\1 [debian/rules:\2]"),
 }
 
 linenos = []

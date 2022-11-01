41af4a235ff97ed5c31ffa380e8a48183f7bc188
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-11-01 18:23:35 +0000

    Bump version.

--- lintian_brush/__init__.py
+++ lintian_brush/__init__.py
@@ -56,7 +56,7 @@ from breezy.workspace import reset_tree, check_clean_tree
 from debmutate.reformatting import FormattingUnpreservable
 
 
-__version__ = (0, 134)
+__version__ = (0, 135)
 version_string = ".".join(map(str, __version__))
 SUPPORTED_CERTAINTIES = ["certain", "confident", "likely", "possible", None]
 DEFAULT_MINIMUM_CERTAINTY = "certain"

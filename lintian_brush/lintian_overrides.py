7b28b43d79222b6b6f418b33a317851efabcb8bc
Jelmer Vernooij <jelmer@jelmer.uk>
2022-11-08 19:25:27 +0000

    Stricter rules for changing lintian overrides.

--- lintian_brush/lintian_overrides.py
+++ lintian_brush/lintian_overrides.py
@@ -227,13 +227,13 @@ LINENO_MATCH = r"\d+|\*"
 
 # "$file (line $lineno)" => "[$file:$lineno]"
 PURE_FLN_SUB = (
-    r"^(?P<path>[^[].*) \(line (?P<lineno>" + LINENO_MATCH + r")\)$",
+    r"^(?P<path>[^[ ].+) \(line (?P<lineno>" + LINENO_MATCH + r")\)$",
     r"[\1:\2]")
 # "$file (line $lineno)" => "* [$file:$lineno]"
 PURE_FLN_WILDCARD_SUB = (
-    r"^(?P<path>.+) \(line (?P<lineno>" + LINENO_MATCH + r")\)$", r"* [\1:\2]")
+    r"^(?P<path>[^ ]+) \(line (?P<lineno>" + LINENO_MATCH + r")\)$", r"* [\1:\2]")
 # "$file" => "[$file]"
-PURE_FN_SUB = (r"^(?P<path>[^[].*)", r"[\1]")
+PURE_FN_SUB = (r"^(?P<path>[^[ ].+)", r"[\1]")
 
 
 # When adding new expressions here, make sure the first argument doesn't match

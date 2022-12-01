d80e877fdd4592c40e4e450bc43429becd85d608
Jelmer Vernooĳ <jelmer@jelmer.uk>
2022-12-01 01:14:25 +0000

    Only remove get-orig-source target in package-only trees in opinionated mode.

--- fixers/debian-rules-contains-unnecessary-get-orig-source-target.py
+++ fixers/debian-rules-contains-unnecessary-get-orig-source-target.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 
 from debmutate._rules import RulesEditor
-from lintian_brush.fixer import report_result, LintianIssue
+from lintian_brush.fixer import report_result, LintianIssue, opinionated
+import os
+import sys
 
 certainty = None
+
+if not opinionated() and os.listdir() == ['debian']:
+    # See https://salsa.debian.org/debian-ayatana-team/snapd-glib/-/merge_requests/6#note_358358
+    sys.exit(0)
 
 try:
     with RulesEditor() as updater:

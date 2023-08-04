8759d66cb95b37b78c81a82a9cad9ada9858d7da
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-08-04 16:42:07 +0100

    Support parse_relations / format_relations

--- src/debmutateshim.rs
+++ src/debmutateshim.rs
@@ -1,5 +1,9 @@
+use debversion::Version;
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
+use pyo3::types::{PyDict, PyTuple};
 use std::path::Path;
+use std::str::FromStr;
 
 pub struct Deb822Paragraph(pub(crate) PyObject);
 
@@ -31,7 +35,7 @@ impl Deb822Paragraph {
 pub struct ControlEditor(pub(crate) PyObject);
 
 impl ControlEditor {
-    pub fn new(path: Option<&Path>) -> Self {
+    pub fn open(path: Option<&Path>) -> Self {
         Python::with_gil(|py| {
             let path = path.map_or_else(|| "debian/control", |p| p.to_str().unwrap());
             let control = py
@@ -95,6 +99,182 @@ impl Drop for ControlEditor {
     }
 }
 
+#[derive(Debug, PartialEq, Eq)]
+pub struct ArchRestriction {
+    pub enabled: bool,
+    pub arch: String,
+}
+
+impl FromPyObject<'_> for ArchRestriction {
+    fn extract(ob: &PyAny) -> PyResult<Self> {
+        let enabled = ob.getattr("enabled")?.extract()?;
+        let arch = ob.getattr("arch")?.extract()?;
+        Ok(ArchRestriction { enabled, arch })
+    }
+}
+
+impl ToPyObject for ArchRestriction {
+    fn to_object(&self, py: Python) -> PyObject {
+        let enabled = self.enabled.to_object(py);
+        let arch = self.arch.to_object(py);
+        let ar_cls = py
+            .import("debmutate.control")
+            .unwrap()
+            .getattr("PkgRelation")
+            .unwrap()
+            .getattr("ArchRestriction")
+            .unwrap();
+        ar_cls.call1((enabled, arch)).unwrap().to_object(py)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct BuildRestriction {
+    pub enabled: bool,
+    pub profile: String,
+}
+
+impl FromPyObject<'_> for BuildRestriction {
+    fn extract(ob: &PyAny) -> PyResult<Self> {
+        let enabled = ob.getattr("enabled")?.extract()?;
+        let profile = ob.getattr("profile")?.extract()?;
+        Ok(BuildRestriction { enabled, profile })
+    }
+}
+
+impl ToPyObject for BuildRestriction {
+    fn to_object(&self, py: Python) -> PyObject {
+        let enabled = self.enabled.to_object(py);
+        let profile = self.profile.to_object(py);
+        let br_cls = py
+            .import("debmutate.control")
+            .unwrap()
+            .getattr("PkgRelation")
+            .unwrap()
+            .getattr("BuildRestriction")
+            .unwrap();
+        br_cls.call1((enabled, profile)).unwrap().to_object(py)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct VersionConstraint {
+    pub operator: String,
+    pub version: Version,
+}
+
+impl VersionConstraint {
+    pub fn check(&self, version: &Version) -> bool {
+        match self.operator.as_str() {
+            "=" => version == &self.version,
+            "<<" => version < &self.version,
+            "<=" => version <= &self.version,
+            ">>" => version > &self.version,
+            ">=" => version >= &self.version,
+            _ => panic!("Unknown operator {}", self.operator),
+        }
+    }
+}
+
+impl FromPyObject<'_> for VersionConstraint {
+    fn extract(ob: &PyAny) -> PyResult<Self> {
+        // Extract operator and version from ob (a tuple)
+        if ob.len()? != 2 {
+            return Err(PyValueError::new_err(
+                "VersionConstraint must be a tuple of length 2",
+            ));
+        }
+        let operator = ob.get_item(0)?.extract()?;
+        let version = ob.get_item(1)?;
+        if let Ok(version) = Version::extract(version) {
+            Ok(VersionConstraint { operator, version })
+        } else {
+            Ok(VersionConstraint {
+                operator,
+                version: version.extract::<String>()?.parse().unwrap(),
+            })
+        }
+    }
+}
+
+impl ToPyObject for VersionConstraint {
+    fn to_object(&self, py: Python) -> PyObject {
+        let operator = self.operator.to_object(py);
+        let version = self.version.to_object(py);
+        PyTuple::new(py, vec![operator, version]).to_object(py)
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParsedRelation {
+    pub name: String,
+    pub archqual: Option<String>,
+    pub version: Option<VersionConstraint>,
+    pub arch: Option<Vec<ArchRestriction>>,
+    pub restrictions: Option<Vec<BuildRestriction>>,
+}
+
+pub struct PkgRelation(Vec<Vec<ParsedRelation>>);
+
+impl FromPyObject<'_> for ParsedRelation {
+    fn extract(ob: &PyAny) -> PyResult<Self> {
+        let name = ob.getattr("name")?.extract()?;
+        let archqual = ob.getattr("archqual")?.extract()?;
+        let version = ob.getattr("version")?.extract()?;
+        let arch = ob.getattr("arch")?.extract()?;
+        let restrictions = ob.getattr("restrictions")?.extract()?;
+        Ok(ParsedRelation {
+            name,
+            archqual,
+            version,
+            arch,
+            restrictions,
+        })
+    }
+}
+
+impl ToPyObject for ParsedRelation {
+    fn to_object(&self, py: Python) -> PyObject {
+        let pr_cls = py
+            .import("debmutate._deb822")
+            .unwrap()
+            .getattr("PkgRelation")
+            .unwrap();
+        let ret = PyDict::new(py);
+        ret.set_item("name", self.name.to_object(py)).unwrap();
+        ret.set_item("archqual", self.archqual.to_object(py))
+            .unwrap();
+        ret.set_item("version", self.version.to_object(py)).unwrap();
+        ret.set_item("arch", self.arch.to_object(py)).unwrap();
+        ret.set_item("restrictions", self.restrictions.to_object(py))
+            .unwrap();
+        pr_cls.call((), Some(ret)).unwrap().to_object(py)
+    }
+}
+
+pub fn format_relations(relations: &[(&str, &[ParsedRelation], &str)]) -> String {
+    Python::with_gil(|py| {
+        let relations = relations.to_object(py);
+        println!("relations: {}", relations);
+        let result = py
+            .import("debmutate.control")
+            .unwrap()
+            .call_method1("format_relations", (relations,));
+        result.unwrap().extract().unwrap()
+    })
+}
+
+pub fn parse_relations(relations: &str) -> Vec<(String, Vec<ParsedRelation>, String)> {
+    Python::with_gil(|py| {
+        let result = py
+            .import("debmutate.control")
+            .unwrap()
+            .call_method1("parse_relations", (relations,))
+            .unwrap();
+        result.extract().unwrap()
+    })
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -104,5 +284,53 @@ mod tests {
         let ce = ControlEditor::create(Some(td.path().join("control").as_path()));
         assert!(ce.source().is_some());
         assert!(ce.binaries().is_empty());
+    }
+
+    #[test]
+    fn parse_relations() {
+        assert_eq!(
+            super::parse_relations("foo (>= 1.0) [amd64]"),
+            vec![(
+                "".to_string(),
+                vec![super::ParsedRelation {
+                    name: "foo".to_string(),
+                    archqual: None,
+                    version: Some(super::VersionConstraint {
+                        operator: ">=".to_string(),
+                        version: "1.0".parse().unwrap()
+                    }),
+                    arch: Some(vec![super::ArchRestriction {
+                        enabled: true,
+                        arch: "amd64".to_string()
+                    }]),
+                    restrictions: None
+                }],
+                "".to_string()
+            )]
+        );
+    }
+
+    #[test]
+    fn format_relations() {
+        assert_eq!(
+            super::format_relations(&[(
+                "",
+                &[super::ParsedRelation {
+                    name: "foo".to_string(),
+                    archqual: None,
+                    version: Some(super::VersionConstraint {
+                        operator: ">=".to_string(),
+                        version: "1.0".parse().unwrap()
+                    }),
+                    arch: Some(vec![super::ArchRestriction {
+                        enabled: true,
+                        arch: "amd64".to_string()
+                    }]),
+                    restrictions: None
+                }],
+                ""
+            )]),
+            "foo (>= 1.0) [amd64]"
+        );
     }
 }

9fa56260d09aa1ef789ee50a0d88e2587cb18ea9
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-09-23 14:27:15 +0100

    determine_browser_url: add some tests

--- src/vcs.rs
+++ src/vcs.rs
@@ -4,9 +4,75 @@ pub fn determine_browser_url(vcs_type: &str, vcs_url: &Url) -> Option<Url> {
     pyo3::Python::with_gil(|py| {
         let vcs = py.import("lintian_brush.vcs").unwrap();
         let cb = vcs.getattr("determine_browser_url").unwrap();
-        let url = vcs.call1((vcs_type, vcs_url.as_str())).unwrap();
+        let url = cb.call1((vcs_type, vcs_url.as_str())).unwrap();
         let url = url.extract::<String>().unwrap();
         let url = Url::parse(&url).ok()?;
         Some(url)
     })
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_determine_browser_url() {
+        use super::determine_browser_url;
+        use url::Url;
+
+        assert_eq!(
+            determine_browser_url(
+                "git",
+                &Url::parse("https://salsa.debian.org/foo/bar").unwrap()
+            ),
+            Some(Url::parse("https://salsa.debian.org/foo/bar").unwrap())
+        );
+        assert_eq!(
+            determine_browser_url(
+                "git",
+                &Url::parse("https://salsa.debian.org/foo/bar.git").unwrap()
+            ),
+            Some(Url::parse("https://salsa.debian.org/foo/bar").unwrap())
+        );
+        assert_eq!(
+            determine_browser_url(
+                "git",
+                &Url::parse("https://salsa.debian.org/foo/bar/").unwrap()
+            ),
+            Some(Url::parse("https://salsa.debian.org/foo/bar").unwrap())
+        );
+        assert_eq!(
+            determine_browser_url(
+                "git",
+                &Url::parse("https://salsa.debian.org/foo/bar/.git").unwrap()
+            ),
+            Some(Url::parse("https://salsa.debian.org/foo/bar/").unwrap())
+        );
+        assert_eq!(
+            determine_browser_url(
+                "git",
+                &Url::parse("https://salsa.debian.org/foo/bar.git/").unwrap()
+            ),
+            Some(Url::parse("https://salsa.debian.org/foo/bar").unwrap())
+        );
+        assert_eq!(
+            determine_browser_url(
+                "git",
+                &Url::parse("https://salsa.debian.org/foo/bar.git/.git").unwrap()
+            ),
+            Some(Url::parse("https://salsa.debian.org/foo/bar.git/").unwrap())
+        );
+        assert_eq!(
+            determine_browser_url(
+                "git",
+                &Url::parse("https://salsa.debian.org/foo/bar.git.git").unwrap()
+            ),
+            Some(Url::parse("https://salsa.debian.org/foo/bar.git").unwrap())
+        );
+        assert_eq!(
+            determine_browser_url(
+                "git",
+                &Url::parse("https://salsa.debian.org/foo/bar.git.git/").unwrap()
+            ),
+            Some(Url::parse("https://salsa.debian.org/foo/bar.git").unwrap())
+        );
+    }
 }

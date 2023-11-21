e36aa356e255a8de1a6cf78d1d3d2bbdc1b233c7
Jelmer Vernooij <jelmer@jelmer.uk>
2023-11-21 13:31:23 +0000

    Start on 0.153

--- lintian_brush/__init__.py
+++ lintian_brush/__init__.py
@@ -39,7 +39,7 @@ from debmutate.reformatting import FormattingUnpreservable
 
 from . import _lintian_brush_rs
 
-__version__ = (0, 152)
+__version__ = (0, 153)
 version_string = ".".join(map(str, __version__))
 SUPPORTED_CERTAINTIES = ["certain", "confident", "likely", "possible", None]
 DEFAULT_MINIMUM_CERTAINTY = "certain"

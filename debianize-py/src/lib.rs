282c1162aca975c5bf5b3d5f87aaaed848321d24
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-02-04 19:00:26 +0000

    Migrate write_template to string

--- debianize-py/src/lib.rs
+++ debianize-py/src/lib.rs
@@ -1,15 +1,30 @@
+use debianize::BugKind;
+use debversion::Version;
 use pyo3::prelude::*;
 
 #[pyfunction]
-fn default_debianize_cache_dir() -> PyResult<String> {
-    Ok(debianize::debfault_debianize_cache_dir()?)
+fn default_debianize_cache_dir() -> PyResult<std::path::PathBuf> {
+    Ok(debianize::default_debianize_cache_dir()?)
+}
+
+#[pyfunction]
+fn write_changelog_template(
+    path: std::path::PathBuf,
+    source_name: &str,
+    version: Version,
+    author: Option<(String, String)>,
+    wnpp_bugs: Option<Vec<(BugKind, u32)>>,
+) -> Result<(), std::io::Error> {
+    debianize::write_changelog_template(path.as_path(), source_name, &version, author, wnpp_bugs)?;
+    Ok(())
 }
 
 #[pymodule]
 fn _debianize_rs(py: Python, m: &PyModule) -> PyResult<()> {
     pyo3_log::init();
 
-    m.add_function(wrap_pyfunction!(debfault_debianize_cache_dir, m)?)?;
+    m.add_function(wrap_pyfunction!(default_debianize_cache_dir, m)?)?;
+    m.add_function(wrap_pyfunction!(write_changelog_template, m)?)?;
 
     Ok(())
 }
--- debianize/src/lib.rs
+++ debianize/src/lib.rs
@@ -1,3 +1,79 @@
+use debversion::Version;
+
 pub fn default_debianize_cache_dir() -> std::io::Result<std::path::PathBuf> {
     xdg::BaseDirectories::with_prefix("debianize")?.create_cache_directory("")
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+pub enum BugKind {
+    RFP,
+    ITP,
+}
+
+impl std::str::FromStr for BugKind {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "RFP" => Ok(BugKind::RFP),
+            "ITP" => Ok(BugKind::ITP),
+            _ => Err(format!("Unknown bug kind: {}", s)),
+        }
+    }
+}
+
+impl ToString for BugKind {
+    fn to_string(&self) -> String {
+        match self {
+            BugKind::RFP => "RFP".to_string(),
+            BugKind::ITP => "ITP".to_string(),
+        }
+    }
+}
+
+#[cfg(feature = "pyo3")]
+impl pyo3::FromPyObject<'_> for BugKind {
+    fn extract(ob: &pyo3::PyAny) -> pyo3::PyResult<Self> {
+        let s: String = ob.extract()?;
+        s.parse()
+            .map_err(|e| pyo3::exceptions::PyValueError::new_err(e))
+    }
+}
+
+pub fn write_changelog_template(
+    path: &std::path::Path,
+    source_name: &str,
+    version: &Version,
+    author: Option<(String, String)>,
+    wnpp_bugs: Option<Vec<(BugKind, u32)>>,
+) -> Result<(), std::io::Error> {
+    let author = author.unwrap_or_else(|| debian_changelog::get_maintainer().unwrap());
+    let closes = if let Some(wnpp_bugs) = wnpp_bugs {
+        format!(
+            " Closes: {}",
+            wnpp_bugs
+                .iter()
+                .map(|(_k, n)| format!("#{}", n))
+                .collect::<Vec<_>>()
+                .join(", ")
+        )
+    } else {
+        "".to_string()
+    };
+    let mut cl = debian_changelog::ChangeLog::new();
+
+    cl.new_entry()
+        .package(source_name.to_string())
+        .version(version.clone())
+        .distribution("UNRELEASED".to_string())
+        .urgency(debian_changelog::Urgency::Low)
+        .change_line(format!("  * Initial release.{}", closes))
+        .maintainer(author)
+        .finish();
+
+    let buf = cl.to_string();
+
+    std::fs::write(path, buf)?;
+
+    Ok(())
 }

fa8c94626bb313a3fbf26642c65ce8e6a651099d
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-08-20 15:37:29 +0100

    Add version convenience functions

--- debianize/src/lib.rs
+++ debianize/src/lib.rs
@@ -2,14 +2,18 @@ use breezyshim::debian::error::Error as BrzDebianError;
 use breezyshim::debian::merge_upstream::{
     do_import, get_existing_imported_upstream_revids, get_tarballs,
 };
-use breezyshim::debian::upstream::{PristineTarSource, UpstreamBranchSource, UpstreamSource};
+use breezyshim::debian::upstream::{
+    upstream_version_add_revision, PristineTarSource, UpstreamBranchSource, UpstreamSource,
+};
 use breezyshim::debian::{TarballKind, DEFAULT_ORIG_DIR};
 use breezyshim::error::Error as BrzError;
 use breezyshim::workingtree::WorkingTree;
 use breezyshim::RevisionId;
+use debian_analyzer::versions::debianize_upstream_version;
 use debversion::Version;
 use std::collections::HashMap;
 use std::path::{Path, PathBuf};
+use upstream_ontologist::UpstreamMetadata;
 
 pub mod simple_apt_repo;
 
@@ -289,6 +293,24 @@ pub fn import_upstream_dist(
 
     let orig_revid = pristine_revids.remove(&TarballKind::Orig).unwrap().0;
     Ok((orig_revid, upstream_branch_name, tag_names))
+}
+
+/// Generate an upstream version for a package if all else fails.
+pub fn last_resort_upstream_version(
+    upstream_source: &UpstreamBranchSource,
+    upstream_revision: &RevisionId,
+) -> Result<String, BrzDebianError> {
+    let upstream_version = upstream_version_add_revision(
+        upstream_source.upstream_branch().as_ref(),
+        "0",
+        upstream_revision,
+        Some("+"),
+    )?;
+    log::warn!(
+        "Unable to determine upstream version, using {}.",
+        upstream_version
+    );
+    Ok(upstream_version)
 }
 
 #[cfg(test)]

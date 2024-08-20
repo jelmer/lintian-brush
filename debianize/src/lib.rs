74b5c4b319b81a961e74ac7829003eb5330b56b5
Jelmer Vernooĳ <jelmer@jelmer.uk>
2024-08-20 02:11:59 +0100

    Add simple apt repo

--- debianize/src/lib.rs
+++ debianize/src/lib.rs
@@ -1,5 +1,7 @@
 use debversion::Version;
 
+pub mod simple_apt_repo;
+
 pub fn default_debianize_cache_dir() -> std::io::Result<std::path::PathBuf> {
     xdg::BaseDirectories::with_prefix("debianize")?.create_cache_directory("")
 }

c9bcb3083ec2905ef03810c690423227b91d8700
Jelmer Vernooij <jelmer@jelmer.uk>
2024-09-10 20:29:35 +0000

    Import names functions from upstream_ontologist

--- debianize/src/names.rs
+++ debianize/src/names.rs
@@ -44,9 +44,59 @@ pub fn python_binary_package_name(upstream_name: &str) -> String {
     format!("python3-{}", upstream_name.replace('_', "-").to_lowercase())
 }
 
+pub fn debian_to_upstream_version(version: &str) -> &str {
+    // Drop debian-specific modifiers from an upstream version string.
+    version.split("+dfsg").next().unwrap_or_default()
+}
+
+pub fn upstream_name_to_debian_source_name(mut upstream_name: &str) -> String {
+    if let Some((_, _, abbrev)) = lazy_regex::regex_captures!(r"^(.{10,})\((.*)\)", upstream_name) {
+        upstream_name = abbrev;
+    }
+
+    // Remove "GNU " prefix
+    if upstream_name.starts_with("GNU ") {
+        upstream_name = &upstream_name["GNU ".len()..];
+    }
+
+    // Convert to lowercase and replace characters
+    upstream_name.to_lowercase().replace(['_', ' ', '/'], "-")
+}
+
+pub fn upstream_package_to_debian_source_name(family: &str, name: &str) -> String {
+    match family {
+        "rust" => format!("rust-{}", name.to_lowercase()),
+        "perl" => format!("lib{}-perl", name.to_lowercase().replace("::", "-")),
+        "node" => format!("node-{}", name.to_lowercase()),
+        _ => upstream_name_to_debian_source_name(name),
+    }
+}
+
+pub fn upstream_package_to_debian_binary_name(family: &str, name: &str) -> String {
+    match family {
+        "rust" => format!("rust-{}", name.to_lowercase()),
+        "perl" => format!("lib{}-perl", name.to_lowercase().replace("::", "-")),
+        "node" => format!("node-{}", name.to_lowercase()),
+        _ => name.to_lowercase().replace('_', "-"),
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_gnu() {
+        assert_eq!("lala", upstream_name_to_debian_source_name("GNU Lala"));
+    }
+
+    #[test]
+    fn test_abbrev() {
+        assert_eq!(
+            "mun",
+            upstream_name_to_debian_source_name("Made Up Name (MUN)")
+        );
+    }
 
     #[test]
     fn test_source_name_from_directory_name() {

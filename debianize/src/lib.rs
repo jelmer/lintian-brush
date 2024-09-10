d6113575e5fa5aa55db1fe93e4e5cd24b3e7afde
Jelmer Vernooij <jelmer@jelmer.uk>
2024-09-10 19:58:13 +0000

    Move names mangling to separate module

--- debianize/src/lib.rs
+++ debianize/src/lib.rs
@@ -15,6 +15,7 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use upstream_ontologist::UpstreamMetadata;
 
+mod names;
 pub mod simple_apt_repo;
 
 pub fn default_debianize_cache_dir() -> std::io::Result<std::path::PathBuf> {
@@ -93,52 +94,6 @@ pub fn write_changelog_template(
     std::fs::write(path, buf)?;
 
     Ok(())
-}
-
-pub fn source_name_from_directory_name(path: &std::path::Path) -> String {
-    let d = path.file_name().unwrap().to_str().unwrap();
-    if d.contains('-') {
-        let mut parts = d.split('-').collect::<Vec<_>>();
-        let c = parts.last().unwrap().chars().next().unwrap();
-        if c.is_ascii_digit() {
-            parts.pop();
-            return parts.join("-");
-        }
-    }
-    d.to_string()
-}
-
-pub fn go_import_path_from_repo(repo_url: &url::Url) -> String {
-    repo_url.host_str().unwrap().to_string()
-        + repo_url
-            .path()
-            .trim_end_matches('/')
-            .trim_end_matches(".git")
-}
-
-pub fn perl_package_name(upstream_name: &str) -> String {
-    let upstream_name = upstream_name.strip_prefix("lib").unwrap_or(upstream_name);
-    format!(
-        "lib{}-perl",
-        upstream_name
-            .replace("::", "-")
-            .replace('_', "")
-            .to_lowercase()
-    )
-}
-
-pub fn python_source_package_name(upstream_name: &str) -> String {
-    let upstream_name = upstream_name
-        .strip_prefix("python-")
-        .unwrap_or(upstream_name);
-    format!("python-{}", upstream_name.replace('_', "-").to_lowercase())
-}
-
-pub fn python_binary_package_name(upstream_name: &str) -> String {
-    let upstream_name = upstream_name
-        .strip_prefix("python-")
-        .unwrap_or(upstream_name);
-    format!("python3-{}", upstream_name.replace('_', "-").to_lowercase())
 }
 
 pub fn use_packaging_branch(wt: &WorkingTree, branch_name: &str) -> Result<(), BrzError> {
@@ -311,83 +266,4 @@ pub fn last_resort_upstream_version(
         upstream_version
     );
     Ok(upstream_version)
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_source_name_from_directory_name() {
-        assert_eq!(
-            source_name_from_directory_name(std::path::Path::new("foo")),
-            "foo"
-        );
-        assert_eq!(
-            source_name_from_directory_name(std::path::Path::new("foo-bar")),
-            "foo-bar"
-        );
-        assert_eq!(
-            source_name_from_directory_name(std::path::Path::new("foo-bar-1")),
-            "foo-bar"
-        );
-        assert_eq!(
-            source_name_from_directory_name(std::path::Path::new("foo-bar-1.0")),
-            "foo-bar"
-        );
-    }
-
-    #[test]
-    fn test_go_import_path_from_repo() {
-        assert_eq!(
-            go_import_path_from_repo(&url::Url::parse("https://github.com/foo/bar.git").unwrap()),
-            "github.com/foo/bar"
-        );
-    }
-
-    #[test]
-    fn test_perl_package_name() {
-        assert_eq!(perl_package_name("Foo::Bar"), "libfoo-bar-perl");
-        assert_eq!(perl_package_name("Foo::Bar::Baz"), "libfoo-bar-baz-perl");
-        assert_eq!(
-            perl_package_name("Foo::Bar::Baz::Qux"),
-            "libfoo-bar-baz-qux-perl"
-        );
-        assert_eq!(
-            perl_package_name("Foo::Bar::Baz::Qux::Quux"),
-            "libfoo-bar-baz-qux-quux-perl"
-        );
-        assert_eq!(
-            perl_package_name("Foo::Bar::Baz::Qux::Quux::Corge"),
-            "libfoo-bar-baz-qux-quux-corge-perl"
-        );
-    }
-
-    #[test]
-    fn test_python_source_package_name() {
-        assert_eq!(python_source_package_name("foo"), "python-foo");
-        assert_eq!(
-            python_source_package_name("python-foo_bar"),
-            "python-foo-bar"
-        );
-        assert_eq!(python_source_package_name("foo_bar"), "python-foo-bar");
-        assert_eq!(
-            python_source_package_name("foo_bar_baz"),
-            "python-foo-bar-baz"
-        );
-    }
-
-    #[test]
-    fn test_python_binary_package_name() {
-        assert_eq!(python_binary_package_name("foo"), "python3-foo");
-        assert_eq!(
-            python_binary_package_name("python-foo_bar"),
-            "python3-foo-bar"
-        );
-        assert_eq!(python_binary_package_name("foo_bar"), "python3-foo-bar");
-        assert_eq!(
-            python_binary_package_name("foo_bar_baz"),
-            "python3-foo-bar-baz"
-        );
-    }
 }
---
+++ debianize/src/names.rs
@@ -0,0 +1,124 @@
+pub fn source_name_from_directory_name(path: &std::path::Path) -> String {
+    let d = path.file_name().unwrap().to_str().unwrap();
+    if d.contains('-') {
+        let mut parts = d.split('-').collect::<Vec<_>>();
+        let c = parts.last().unwrap().chars().next().unwrap();
+        if c.is_ascii_digit() {
+            parts.pop();
+            return parts.join("-");
+        }
+    }
+    d.to_string()
+}
+
+pub fn go_import_path_from_repo(repo_url: &url::Url) -> String {
+    repo_url.host_str().unwrap().to_string()
+        + repo_url
+            .path()
+            .trim_end_matches('/')
+            .trim_end_matches(".git")
+}
+
+pub fn perl_package_name(upstream_name: &str) -> String {
+    let upstream_name = upstream_name.strip_prefix("lib").unwrap_or(upstream_name);
+    format!(
+        "lib{}-perl",
+        upstream_name
+            .replace("::", "-")
+            .replace('_', "")
+            .to_lowercase()
+    )
+}
+
+pub fn python_source_package_name(upstream_name: &str) -> String {
+    let upstream_name = upstream_name
+        .strip_prefix("python-")
+        .unwrap_or(upstream_name);
+    format!("python-{}", upstream_name.replace('_', "-").to_lowercase())
+}
+
+pub fn python_binary_package_name(upstream_name: &str) -> String {
+    let upstream_name = upstream_name
+        .strip_prefix("python-")
+        .unwrap_or(upstream_name);
+    format!("python3-{}", upstream_name.replace('_', "-").to_lowercase())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_source_name_from_directory_name() {
+        assert_eq!(
+            source_name_from_directory_name(std::path::Path::new("foo")),
+            "foo"
+        );
+        assert_eq!(
+            source_name_from_directory_name(std::path::Path::new("foo-bar")),
+            "foo-bar"
+        );
+        assert_eq!(
+            source_name_from_directory_name(std::path::Path::new("foo-bar-1")),
+            "foo-bar"
+        );
+        assert_eq!(
+            source_name_from_directory_name(std::path::Path::new("foo-bar-1.0")),
+            "foo-bar"
+        );
+    }
+
+    #[test]
+    fn test_go_import_path_from_repo() {
+        assert_eq!(
+            go_import_path_from_repo(&url::Url::parse("https://github.com/foo/bar.git").unwrap()),
+            "github.com/foo/bar"
+        );
+    }
+
+    #[test]
+    fn test_perl_package_name() {
+        assert_eq!(perl_package_name("Foo::Bar"), "libfoo-bar-perl");
+        assert_eq!(perl_package_name("Foo::Bar::Baz"), "libfoo-bar-baz-perl");
+        assert_eq!(
+            perl_package_name("Foo::Bar::Baz::Qux"),
+            "libfoo-bar-baz-qux-perl"
+        );
+        assert_eq!(
+            perl_package_name("Foo::Bar::Baz::Qux::Quux"),
+            "libfoo-bar-baz-qux-quux-perl"
+        );
+        assert_eq!(
+            perl_package_name("Foo::Bar::Baz::Qux::Quux::Corge"),
+            "libfoo-bar-baz-qux-quux-corge-perl"
+        );
+    }
+
+    #[test]
+    fn test_python_source_package_name() {
+        assert_eq!(python_source_package_name("foo"), "python-foo");
+        assert_eq!(
+            python_source_package_name("python-foo_bar"),
+            "python-foo-bar"
+        );
+        assert_eq!(python_source_package_name("foo_bar"), "python-foo-bar");
+        assert_eq!(
+            python_source_package_name("foo_bar_baz"),
+            "python-foo-bar-baz"
+        );
+    }
+
+    #[test]
+    fn test_python_binary_package_name() {
+        assert_eq!(python_binary_package_name("foo"), "python3-foo");
+        assert_eq!(
+            python_binary_package_name("python-foo_bar"),
+            "python3-foo-bar"
+        );
+        assert_eq!(python_binary_package_name("foo_bar"), "python3-foo-bar");
+        assert_eq!(
+            python_binary_package_name("foo_bar_baz"),
+            "python3-foo-bar-baz"
+        );
+    }
+}

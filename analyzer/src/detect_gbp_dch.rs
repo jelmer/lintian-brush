2f38705aecd007ae42bb44878436007443e70e39
Jelmer Vernooij <jelmer@jelmer.uk>
2024-09-25 18:27:09 +0000

    Revert all_sha_prefixed change; tests failing

--- analyzer/src/detect_gbp_dch.rs
+++ analyzer/src/detect_gbp_dch.rs
@@ -4,6 +4,7 @@ use breezyshim::graph::{Error as GraphError, Graph};
 use breezyshim::revisionid::RevisionId;
 use breezyshim::tree::{Tree, WorkingTree};
 use debian_changelog::{ChangeLog, Entry as ChangeLogEntry};
+use lazy_regex::regex;
 
 #[derive(Debug, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
 pub struct ChangelogBehaviour {
@@ -313,14 +314,19 @@ pub fn guess_update_changelog_from_branch(
 ///
 /// * `cl` - Changelog entry
 pub fn all_sha_prefixed(cb: &ChangeLogEntry) -> bool {
-    let changes = cb.change_lines().collect::<Vec<_>>();
-    debian_changelog::changes::all_sha_prefixed(
-        changes
-            .iter()
-            .map(|x| x.as_str())
-            .collect::<Vec<_>>()
-            .as_slice(),
-    )
+    let mut sha_prefixed = 0;
+    for change in cb.change_lines() {
+        if !change.starts_with("* ") {
+            continue;
+        }
+        if regex!(r"\* \[[0-9a-f]{7}\] ").is_match(change.as_str()) {
+            sha_prefixed += 1;
+        } else {
+            return false;
+        }
+    }
+
+    sha_prefixed > 0
 }
 
 #[cfg(test)]

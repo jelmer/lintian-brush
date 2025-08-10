ac042d609902f39073e0c3a63e33cb6384173f10
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-08-10 02:25:28 +0100

    Migrate debian-pycompat-is-obsolete fixer to Rust
    Simple fixer that removes the obsolete debian/pycompat file.

--- lintian-brush/src/fixers/mod.rs
+++ lintian-brush/src/fixers/mod.rs
@@ -1,5 +1,6 @@
 pub mod build_depends_on_build_essential;
 pub mod crlf_line_endings;
+pub mod debian_pycompat_is_obsolete;
 pub mod executable_desktop_file;
 pub mod out_of_date_copyright_format_uri;
 pub mod quilt_series_without_trailing_newline;

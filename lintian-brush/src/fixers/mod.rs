160a52a06a1a1eaf5e91a8f86026b1e27e6f7db6
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-08-22 16:25:47 +0100

    Port wrong-debian-qa-group-name fixer to Rust

--- lintian-brush/src/fixers/mod.rs
+++ lintian-brush/src/fixers/mod.rs
@@ -30,5 +30,6 @@ pub mod rules_not_executable;
 pub mod unversioned_copyright_format_uri;
 pub mod upstream_metadata_in_native_source;
 pub mod vcs_field_not_canonical;
+pub mod wrong_debian_qa_group_name;
 pub mod xc_package_type_in_debian_control;
 pub mod xs_testsuite_field_in_debian_control;

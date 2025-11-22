a777f774e0951526e43347b2b7f920a83a3ae0c0
Jelmer Vernooĳ <jelmer@debian.org>
2025-11-22 21:44:15 +0000

    Remove obsolete shell script fixers
    All of these fixers have been ported to Rust and are no longer needed:
    - copyright-has-crs
    - debian-changelog-file-contains-obsolete-user-emacs-settings
    - debian-pycompat-is-obsolete
    - desktop-entry-file-has-crs
    - old-fsf-address-in-copyright-file
    - out-of-date-copyright-format-uri
    - package-uses-deprecated-source-override-location
    - public-upstream-keys-in-multiple-locations
    - quilt-series-without-trailing-newline
    - xc-package-type-in-debian-control
    - xs-testsuite-field-in-debian-control

--- lintian-brush/fixers/xs-testsuite-field-in-debian-control.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-perl -p -i -e 's/XS-Testsuite: autopkgtest\n//' debian/control*
-perl -p -i -e 's/XS-Testsuite: (.*)\n/Testsuite: \1\n/' debian/control*
-echo "Remove unnecessary XS-Testsuite field in debian/control."
-echo "Fixed-Lintian-Tags: adopted-extended-field"

454f73386728b9f1ae4f8078d1b2d16718ec6dc4
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-11-26 12:42:48 +0000

    Port vcs-field-mismatch fixer to Rust

--- lintian-brush/src/fixers/mod.rs
+++ lintian-brush/src/fixers/mod.rs
@@ -61,6 +61,7 @@ pub mod unnecessary_testsuite_autopkgtest_field;
 pub mod unversioned_copyright_format_uri;
 pub mod upstream_metadata_in_native_source;
 pub mod uses_deprecated_adttmp;
+pub mod vcs_field_mismatch;
 pub mod vcs_field_not_canonical;
 pub mod vcs_field_uses_not_recommended_uri_format;
 pub mod wrong_debian_qa_group_name;

133fc31c1b2f50571390d001cee7f99c04ea4da1
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-11-27 03:54:21 +0000

    Port pkg-perl-vcs fixer to Rust

--- lintian-brush/src/fixers/mod.rs
+++ lintian-brush/src/fixers/mod.rs
@@ -69,6 +69,7 @@ pub mod out_of_date_standards_version;
 pub mod package_contains_linda_override;
 pub mod package_uses_deprecated_source_override_location;
 pub mod pkg_perl_testsuite;
+pub mod pkg_perl_vcs;
 pub mod priority_extra_is_replaced_by_priority_optional;
 pub mod public_upstream_key_binary;
 pub mod public_upstream_key_in_native_package;

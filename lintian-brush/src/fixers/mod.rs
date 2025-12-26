a9e8c288bda1963f3349ac36771ef081ba3c7aa3
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-12-26 22:30:05 +0000

    Port vcs-field-bitrotted fixer from Python to Rust

--- lintian-brush/src/fixers/mod.rs
+++ lintian-brush/src/fixers/mod.rs
@@ -144,6 +144,7 @@ mod upstream_metadata_in_native_source;
 mod useless_autoreconf_build_depends;
 mod uses_debhelper_compat_file;
 mod uses_deprecated_adttmp;
+mod vcs_field_bitrotted;
 mod vcs_field_for_maintainer;
 mod vcs_field_invalid_branch;
 mod vcs_field_mismatch;

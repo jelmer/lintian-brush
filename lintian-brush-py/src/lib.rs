c8d0b3865736cbfaa7350a0383b8c77b2fc3910a
Jelmer Vernooĳ <jelmer@jelmer.uk>
2025-12-30 13:37:55 +0000

    Remove unused rust bindings

--- lintian-brush-py/src/lib.rs
+++ lintian-brush-py/src/lib.rs
@@ -2,11 +2,6 @@ use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 
 #[pyfunction]
-fn guess_repository_url(package: &str, maintainer_email: &str) -> Option<String> {
-    debian_analyzer::salsa::guess_repository_url(package, maintainer_email).map(|u| u.to_string())
-}
-
-#[pyfunction]
 #[pyo3(signature = (vcs_type, vcs_url, net_access=None))]
 fn determine_browser_url(
     vcs_type: &str,
@@ -56,19 +51,6 @@ fn find_patches_directory(
     debian_analyzer::patches::find_patches_directory(&tree, subpath.as_path())
 }
 
-#[pyfunction]
-#[pyo3(signature = (tree, patches_directory=None))]
-fn tree_has_non_patches_changes(
-    tree: Py<PyAny>,
-    patches_directory: Option<std::path::PathBuf>,
-) -> PyResult<bool> {
-    let tree = breezyshim::workingtree::GenericWorkingTree(tree);
-    Ok(
-        !debian_analyzer::patches::tree_non_patches_changes(tree, patches_directory.as_deref())?
-            .is_empty(),
-    )
-}
-
 #[pymodule]
 fn _lintian_brush_rs(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     pyo3_log::init();
@@ -78,8 +60,6 @@ fn _lintian_brush_rs(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
         .collect();
     let v = PyTuple::new(py, &version_parts)?;
     m.add("__version__", &v)?;
-    m.add_wrapped(wrap_pyfunction!(tree_has_non_patches_changes))?;
-    m.add_wrapped(wrap_pyfunction!(guess_repository_url))?;
     m.add_wrapped(wrap_pyfunction!(determine_browser_url))?;
     m.add_wrapped(wrap_pyfunction!(determine_gitlab_browser_url))?;
     m.add_wrapped(wrap_pyfunction!(canonicalize_vcs_browser_url))?;

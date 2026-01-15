2f1f0757e8af0443f2fd57fad0173f2e0171c3d5
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-01-15 13:04:18 +0000

    Trim unused exports from lintian-brush-py crate

--- lintian-brush-py/src/lib.rs
+++ lintian-brush-py/src/lib.rs
@@ -2,19 +2,6 @@ use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 
 #[pyfunction]
-#[pyo3(signature = (vcs_type, vcs_url, net_access=None))]
-fn determine_browser_url(
-    vcs_type: &str,
-    vcs_url: &str,
-    net_access: Option<bool>,
-) -> PyResult<Option<String>> {
-    Ok(
-        debian_analyzer::vcs::determine_browser_url(vcs_type, vcs_url, net_access)
-            .map(|u| u.to_string()),
-    )
-}
-
-#[pyfunction]
 fn get_builtin_fixer_lintian_tags() -> Vec<String> {
     lintian_brush::builtin_fixers::get_builtin_fixers()
         .iter()
@@ -31,15 +18,6 @@ fn _lintian_brush_rs(py: Python, m: &Bound<PyModule>) -> PyResult<()> {
         .collect();
     let v = PyTuple::new(py, &version_parts)?;
     m.add("__version__", &v)?;
-    m.add_wrapped(wrap_pyfunction!(determine_browser_url))?;
-    m.add(
-        "DPKG_VERSIONS",
-        debian_analyzer::release_info::dpkg_versions.clone(),
-    )?;
-    m.add(
-        "DEBHELPER_VERSIONS",
-        debian_analyzer::release_info::debhelper_versions.clone(),
-    )?;
     m.add_wrapped(wrap_pyfunction!(get_builtin_fixer_lintian_tags))?;
     Ok(())
 }
--- py/lintian_brush/_lintian_brush_rs.pyi
+++ py/lintian_brush/_lintian_brush_rs.pyi
@@ -1,12 +1,4 @@
 # Stub file for lintian_brush._lintian_brush_rs
 
-from typing import Any
-
-DEBHELPER_VERSIONS: dict[str, Any]
-DPKG_VERSIONS: dict[str, Any]
-
-# Based on usage in vcs.py
-def determine_browser_url(url: str) -> str | None: ...
-
 # Based on usage in __init__.py
 def get_builtin_fixer_lintian_tags() -> list[str]: ...
